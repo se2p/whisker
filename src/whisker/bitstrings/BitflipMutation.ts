0a66d4b8f80ce2d7c336ef150d0d2ebdb4f89809
Gordon Fraser <gordon.fraser@uni-passau.de>
2020-04-10 09:49:57 +0200

    Bitstring representation and examples

---
+++ src/whisker/bitstrings/BitflipMutation.ts
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2020 Whisker contributors
+ *
+ * This file is part of the Whisker test generator for Scratch.
+ *
+ * Whisker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Whisker is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Whisker. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {BitstringChromosome} from './BitstringChromosome';
+import {Mutation} from '../search/Mutation';
+import {List} from '../utils/List';
+import {Randomness} from "../utils/Randomness";
+
+
+export class BitflipMutation implements Mutation<BitstringChromosome> {
+
+    apply (chromosome: BitstringChromosome): BitstringChromosome {
+        const oldBits = chromosome.getBits(); // TODO: Immutable list
+        const newBits = new List<Boolean>();
+        const mutationProbability = 1.0 / oldBits.size();
+
+        for (let i = 0; i < oldBits.size(); i++) {
+            const bit = oldBits.get(i); // TODO: Implement iterator in List
+            if (Randomness.getInstance().nextDouble() < mutationProbability) {
+                newBits.add(!bit);
+            } else {
+                newBits.add(bit);
+            }
+        }
+
+        return new BitstringChromosome(newBits);
+    }
+}
---
+++ src/whisker/bitstrings/OneMaxFitnessFunction.ts
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2020 Whisker contributors
+ *
+ * This file is part of the Whisker test generator for Scratch.
+ *
+ * Whisker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Whisker is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Whisker. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {FitnessFunction} from '../search/FitnessFunction';
+import {BitstringChromosome} from './BitstringChromosome';
+import {Preconditions} from "../utils/Preconditions";
+
+
+export class OneMaxFitnessFunction implements FitnessFunction<BitstringChromosome> {
+
+    private _size: number;
+
+    constructor(size: number) {
+        this._size = size;
+    }
+
+    getFitness (chromosome: BitstringChromosome): number {
+        const bits = chromosome.getBits();
+        Preconditions.checkListSize(bits, this._size);
+
+        let numOnes = 0;
+
+        // This whole loop could probably be summarised in a single line
+        // if we used an Array and some map/apply construct
+        for (let i = 0; i < bits.size(); i++) {
+            const bit = bits.get(i); // TODO: Implement iterator in List
+
+            if (bit) {
+                numOnes++;
+            }
+        }
+
+        return numOnes;
+    }
+
+    compare (value1: number, value2: number): number {
+        // Larger fitness values are better
+        return value2 - value1;
+    }
+
+    isOptimal(fitnessValue: number): boolean {
+        return fitnessValue == this._size;
+    }
+}
