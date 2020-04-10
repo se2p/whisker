c2f5cc453c8c0e634d6b4cc94705c00b2caed1e3
Gordon Fraser <gordon.fraser@uni-passau.de>
2020-04-10 09:47:47 +0200

    Some basic helper functions for List

--- src/whisker/utils/List.ts
+++ src/whisker/utils/List.ts
@@ -2,26 +2,26 @@
  * Copyright (C) 2020 Whisker contributors
  *
  * This file is part of the Whisker test generator for Scratch.
- * 
+ *
  * Whisker is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Whisker is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Whisker.
  * If not, see http://www.gnu.org/licenses/.
- * 
+ *
  */
 
 /**
  * A class to store a list of elements of the same type.
- * 
+ *
  * @param <T> The type of the list elements
  * @author Sophia Geserer
  */
@@ -64,11 +64,26 @@ export class List<T> {
     }
 
     /**
+     * Appends the specified element to the end of this list.
+     * @param element element to be added to the list
+     */
+    addAll(elements: T[]): void {
+        this._items = this._items.concat(elements) // TODO: Nicer way to do this?
+    }
+
+    /**
      * Returns the element at the specified position in this list.
      * @param index index of the element to return
      * @returns the element at the specified position in the list
      */
     get(index: number): T {
         return this._items[index];
+    }
+
+    /**
+     * Remove all elements in the list
+     */
+    clear() {
+        this._items = [];
     }
 }
