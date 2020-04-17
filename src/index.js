2c97c52884b6dcf7224f3ece5eaf2bcd95aa3bad
Christoph Frädrich <fraedric@fim.uni-passau.de>
2020-04-17 08:49:52 +0200

    Integrate typescript files with Whisker-Web

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ const WhiskerUtil = require('./test/whisker-util');
 const TAP13Listener = require('./test-runner/tap13-listener');
 const CoverageGenerator = require('./coverage/coverage');
 const Util = require('./vm/util');
+const Search = require("./whisker/Search")
 
 module.exports = {
     Test,
     TestRunner,
     TAP13Listener,
+    Search,
     WhiskerUtil,
     CoverageGenerator,
     Util
