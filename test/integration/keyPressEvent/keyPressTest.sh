df7b44dec33d25685fd381be6ed548d21b8636b9
Christoph Frädrich <fraedric@fim.uni-passau.de>
2020-05-13 15:41:51 +0200

    Add tests for remaining events

---
+++ test/integration/keyPressEvent/keyPressTest.sh
@@ -0,0 +1,2 @@
+cd ../../../servant
+node servant.js -d -g -s KeyPressEventTest.sb3 -c ../../whisker-main/config/default.json -k
---
+++ test/integration/mouseMoveEvent/mouseMoveTest.sh
@@ -0,0 +1,2 @@
+cd ../../../servant
+node servant.js -d -g -s MouseMoveEventTest.sb3 -c ../../whisker-main/config/default.json -k
