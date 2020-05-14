cc7cdf82924b211c146891ea571f10927b2a3a4b
Christoph Frädrich <fraedric@fim.uni-passau.de>
2020-05-14 15:05:31 +0200

    Adjust test scripts

--- test/integration/keyDownEvent/keyDownTest.sh
+++ test/integration/keyDownEvent/keyDownTest.sh
@@ -1,2 +1,8 @@
+#!/bin/bash 
+
+SCRATCH_FILE=KeyDownEventTest.sb3
+SCRATCH_PATH="$(cd "$(dirname "$SCRATCH_FILE")"; pwd -P)/$(basename "$SCRATCH_FILE")"
+echo $SCRATCH_PATH
+
 cd ../../../servant
-node servant.js -d -g -s KeyDownEventTest.sb3 -c ../../whisker-main/config/default.json -k
+node servant.js -d -g -s $SCRATCH_PATH -c ../../whisker-main/config/default.json -k
