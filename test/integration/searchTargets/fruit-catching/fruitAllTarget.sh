300b663f27202e33dcbeaaa47e46392605d1263c
Christoph Frädrich <fraedric@fim.uni-passau.de>
2020-06-05 09:21:45 +0200

    Add missing test programs

---
+++ test/integration/searchTargets/fruit-catching/fruitAllTarget.sh
@@ -0,0 +1,8 @@
+SCRATCH_FILE=Fruit-Catching-Simplified.sb3
+SCRATCH_PATH="$(cd "$(dirname "$SCRATCH_FILE")"; pwd -P)/$(basename "$SCRATCH_FILE")"
+
+CONFIG_FILE=../default.json
+CONFIG_PATH="$(cd "$(dirname "$CONFIG_FILE")"; pwd -P)/$(basename "$CONFIG_FILE")"
+
+cd ../../../../servant
+node servant.js -d -g -s $SCRATCH_PATH -c $CONFIG_PATH -k -a 8
