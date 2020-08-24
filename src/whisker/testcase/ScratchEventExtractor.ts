67ea9eae3cec03dcdaf2b48ce352b61c546525b9
Sophia Geserer <geserer@fim.uni-passau.de>
2020-08-24 18:20:04 +0200

    Extraction of the possible text snippets --- Refactoring of the event extract method

--- src/whisker/testcase/ScratchEventExtractor.ts
+++ src/whisker/testcase/ScratchEventExtractor.ts
@@ -35,12 +35,17 @@ import {Randomness} from "../utils/Randomness";
 
 export class ScratchEventExtractor {
 
+    private static availableTextSnippets = new List<string>();
+
     static extractEvents(vm: VirtualMachine): List<ScratchEvent> {
         const eventList = new List<ScratchEvent>();
         for (const target of vm.runtime.targets) {
             if (target.hasOwnProperty('blocks')) {
                 for (const blockId of Object.keys(target.blocks._blocks)) {
-                    eventList.addList(this._extractEventsFromBlock(target, target.blocks.getBlock(blockId)));
+                    const event: ScratchEvent = this._extractEventsFromBlock(target, target.blocks.getBlock(blockId));
+                    if (event != null) {
+                        eventList.add(event);
+                    }
                 }
             }
         }
@@ -50,68 +55,88 @@ export class ScratchEventExtractor {
         return eventList;
     }
 
+    /**
+     * Collects all available text snippets that can be used for generating answers.
+     */
+    static extractAvailableTextSnippets(vm: VirtualMachine): void {
+        this.availableTextSnippets = new List<string>();
+        for (const target of vm.runtime.targets) {
+            if (target.hasOwnProperty('blocks')) {
+                for (const blockId of Object.keys(target.blocks._blocks)) {
+                    const snippet = this._extractAvailableTextSnippets(target, target.blocks.getBlock(blockId));
+                    if (snippet != '' && !this.availableTextSnippets.contains(snippet))
+                        this.availableTextSnippets.add(snippet);
+                }
+            }
+        }
+    }
+
     // TODO: How to handle event parameters?
-    static _extractEventsFromBlock(target, block): List<ScratchEvent> {
-        const eventList = new List<ScratchEvent>();
+    static _extractEventsFromBlock(target, block): ScratchEvent {
         const fields = target.blocks.getFields(block);
         if (typeof block.opcode === 'undefined') {
-            return eventList;
+            return null;
         }
 
         switch (target.blocks.getOpcode(block)) {
             case 'event_whenkeypressed': // Key press
-                eventList.add(new KeyPressEvent(fields.KEY_OPTION.value));
-                // one event per concrete key for which there is a hat block
-                break;
+                return new KeyPressEvent(fields.KEY_OPTION.value);
+            // one event per concrete key for which there is a hat block
             case 'sensing_keyoptions': // Key down
-                eventList.add(new KeyDownEvent(fields.KEY_OPTION.value));
-                break;
+                return new KeyDownEvent(fields.KEY_OPTION.value);
             case 'sensing_mousex':
             case 'sensing_mousey':
             case 'touching-mousepointer': // TODO fix block name
                 // Mouse move
-                eventList.add(new MouseMoveEvent()); // TODO: Any hints on position?
-                break;
+                return new MouseMoveEvent(); // TODO: Any hints on position?
             case 'sensing_mousedown':
                 // Mouse down
-                eventList.add(new MouseDownEvent()); // TODO: Any hints on position?
-                break;
+                return new MouseDownEvent(); // TODO: Any hints on position?
             case 'sensing_askandwait':
                 // Type text
                 // TODO: Only if actually asking
                 // TODO: Text length with random length?
-                eventList.add(new TypeTextEvent(this._randomText(3))); // TODO: Any hints on text?
-                break;
+                return new TypeTextEvent(this._randomText(3)); // TODO: Any hints on text?
             case 'event_whenthisspriteclicked':
                 // Click sprite
-                let clickEvent: ClickSpriteEvent = new ClickSpriteEvent(target);
-                eventList.add(clickEvent); // TODO: Store which sprite
-                // TODO: Add one event for each clone of this sprite --- each target is a clone
-                break;
+                return new ClickSpriteEvent(target); // TODO: Store which sprite
+            // TODO: Add one event for each clone of this sprite --- each target is a clone
             case 'event_whenstageclicked':
                 // Click stage
-                eventList.add(new ClickStageEvent(target)); // TODO: Do we need a position for this?
-                break;
+                return new ClickStageEvent(target); // TODO: Do we need a position for this?
             case 'event_whengreaterthan':
                 // Sound
-                eventList.add(new SoundEvent()); // TODO: Volume as parameter
-                break;
+                return new SoundEvent(); // TODO: Volume as parameter
             case 'event_whenlessthan':
                 // Wait duration
-                eventList.add(new WaitEvent()); // TODO: Duration as parameter
-                break;
+                return new WaitEvent(); // TODO: Duration as parameter
         }
-
-        return eventList;
+        return null;
     }
 
     private static _randomText(length: number): string {
-        let answer: string = '';
-        const chars: string = '0123456789abcdefghijklmnopqrstuvwxyzABCDDEFGHIJKLMNOPQRSTUVWXYZ';
+        let answer = '';
+        const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDDEFGHIJKLMNOPQRSTUVWXYZ';
         for (let i = 0; i < length; i++) {
             answer += chars.charAt(Randomness.getInstance().nextInt(0, chars.length - 1));
         }
 
         return answer;
+    }
+
+    static _extractAvailableTextSnippets(target, block): string {
+        let availableTextSnippet = '';
+        if (target.blocks.getOpcode(block) == 'operator_equals') {
+            const inputs = target.blocks.getInputs(block);
+            const op1 = target.blocks.getBlock(inputs.OPERAND1.block);
+            const op2 = target.blocks.getBlock(inputs.OPERAND2.block);
+
+            if (target.blocks.getOpcode(op2) === 'sensing_answer' && target.blocks.getOpcode(op1) === 'text') {
+                availableTextSnippet = target.blocks.getFields(op1).TEXT.value;
+            } else if (target.blocks.getOpcode(op1) === 'sensing_answer' && target.blocks.getOpcode(op2) === 'text') {
+                availableTextSnippet = target.blocks.getFields(op2).TEXT.value;
+            }
+        }
+        return availableTextSnippet;
     }
 }
