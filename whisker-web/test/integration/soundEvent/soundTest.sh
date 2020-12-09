0c668700eee0cc62360c944129e4b5a4e72dbc21
UssEva <40119154+UssEva@users.noreply.github.com>
2020-12-09 20:15:11 +0100

    Update bash files

--- whisker-web/test/integration/soundEvent/soundTest.sh
+++ whisker-web/test/integration/soundEvent/soundTest.sh
@@ -3,8 +3,8 @@
 SCRATCH_FILE=SoundEventTest.sb3
 SCRATCH_PATH="$(cd "$(dirname "$SCRATCH_FILE")"; pwd -P)/$(basename "$SCRATCH_FILE")"
 
-CONFIG_FILE=../../../../whisker-main/config/default.json
+CONFIG_FILE=../../../../config/default.json
 CONFIG_PATH="$(cd "$(dirname "$CONFIG_FILE")"; pwd -P)/$(basename "$CONFIG_FILE")"
 
-cd ../../../servant
+cd ../../../../servant
 node servant.js -d -g -s $SCRATCH_PATH -c $CONFIG_PATH -k
