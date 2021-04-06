27b7f9a31bf71f9e2716bb8760c67c4a9f861b2f
GoetzKat <goetz_katharina@hotmail.de>
2021-04-06 10:52:18 +0200

    Pipeline issue with file address(2)

--- whisker-main/test/whisker/model/util/ModelLoaderXML.test.ts
+++ whisker-main/test/whisker/model/util/ModelLoaderXML.test.ts
@@ -2,14 +2,14 @@
 import {readFileSync} from 'fs';
 import {ModelLoaderXML} from "../../../../src/whisker/model/util/ModelLoaderXML";
 
-const text = readFileSync('./SimpleGraph.graphml', 'utf8');
+const text = readFileSync('test/whisker/model/util/SimpleGraph.graphml', 'utf8');
 const loader = new ModelLoaderXML();
 const model = loader.loadModels(text);
 
 console.log(model);
 
 // todo test model
-const textXML = readFileSync('./SimpleGraph.xml', 'utf8');
+const textXML = readFileSync('test/whisker/model/util/SimpleGraph.xml', 'utf8');
 const modelXML =  loader.loadModels(textXML);
 
 // todo compare the two models?
