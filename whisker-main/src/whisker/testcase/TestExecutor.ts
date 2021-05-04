11553d08e20f420d1bf1befd597c708cab6fe9a0
Patric Feldmeier <feldme04@ads.uni-passau.de>
2021-05-04 15:35:30 +0200

    Fix DirectEncoding in TestExecutor

--- whisker-main/src/whisker/testcase/TestExecutor.ts
+++ whisker-main/src/whisker/testcase/TestExecutor.ts
@@ -40,7 +40,7 @@ export class TestExecutor {
 
     private readonly _vm: VirtualMachine;
     private _vmWrapper: VMWrapper
-    private eventMap: Map<string, List<ScratchEvent>>;
+    private availableEvents: List<ScratchEvent>;
     private eventObservers: EventObserver[] = [];
     private _initialState = {};
     private _projectRunning: boolean;
@@ -48,7 +48,6 @@ export class TestExecutor {
     constructor(vmWrapper: VMWrapper) {
         this._vmWrapper = vmWrapper;
         this._vm = vmWrapper.vm;
-        this.eventMap = new Map<string, List<ScratchEvent>>();
         this.recordInitialState();
     }
 
@@ -74,27 +73,16 @@ export class TestExecutor {
 
         while (this._projectRunning && numCodon < codons.size()) {
             // Fetch the currently available events and save them in the EventMap
-            this.createEventMap(ScratchEventExtractor.extractEvents(this._vm));
-            const eventTypes = new List<string>(Array.from(this.eventMap.keys()));
+            this.availableEvents = ScratchEventExtractor.extractEvents(this._vm);
 
-            if (eventTypes.isEmpty()) {
+            if (this.availableEvents.isEmpty()) {
                 console.log("Whisker-Main: No events available for project.");
                 continue;
             }
-            // Select the next Type of Event
-            const nextEventType: string = eventTypes.get(codons.get(numCodon) % eventTypes.size())
-            numCodon++;
-            // Select the concrete Event from the available Events.
-            // Only use an additional codon if theres actually a selection to make
-            const concreteEvents = this.eventMap.get(nextEventType);
-            let nextEvent: ScratchEvent;
-            if (concreteEvents.size() == 1) {
-                nextEvent = concreteEvents.get(0);
-            } else {
-                nextEvent = concreteEvents.get(codons.get(numCodon % codons.size()) % concreteEvents.size());
-                numCodon++;
-            }
-            const args = this._getArgs(nextEvent, codons, numCodon);
+            // Select the next Event and set its parameter
+            const nextEvent: ScratchEvent = this.availableEvents.get(codons.get(numCodon) % this.availableEvents.size())
+            nextEvent.setParameter(codons, numCodon);
+            const args = nextEvent.getParameter();
             events.add([nextEvent, args]);
             numCodon += nextEvent.getNumParameters() + 1;
             this.notify(nextEvent, args);
@@ -117,37 +105,6 @@ export class TestExecutor {
         this.resetState();
         StatisticsCollector.getInstance().numberFitnessEvaluations++;
         return testChromosome.trace;
-    }
-
-    /**
-     * Creates a map mapping EventTypes to concrete Events which can be executed by the stepper.
-     * @param events the List of concrete Events which should be sorted according to their types.
-     */
-    private createEventMap(events: List<ScratchEvent>): void {
-        // Clear the map from a previous VM-State and refill it with the events of the current State
-        this.eventMap.clear();
-        for (const event of events) {
-            if (!this.eventMap.has(event.constructor.name)) {
-                this.eventMap.set(event.constructor.name, new List<ScratchEvent>([event]));
-            } else {
-                const eventList = this.eventMap.get(event.constructor.name);
-                eventList.add(event);
-                this.eventMap.set(event.constructor.name, eventList);
-            }
-        }
-    }
-
-    private _getArgs(event: ScratchEvent, codons: List<number>, codonPosition: number): number[] {
-        const args = [];
-        for (let i = 0; i < event.getNumParameters(); i++) {
-            // Get next codon, but wrap around if length exceeded
-            const codon = codons.get(++codonPosition % codons.size());
-
-            // TODO: How to map from codon to parameter value?
-            // TODO: Make this responsibility of event?
-            args.push(codon)
-        }
-        return args;
     }
 
     public attach(observer: EventObserver): void {
