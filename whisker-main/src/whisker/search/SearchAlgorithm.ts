e79a7d0d8da83765114570d1e45d06800e2c382e
Patric Feldmeier <patric.feldmeier@uni-passau.de>
2021-06-21 10:52:34 +0200

    Refactoring Part II

--- whisker-main/src/whisker/search/SearchAlgorithm.ts
+++ whisker-main/src/whisker/search/SearchAlgorithm.ts
@@ -90,8 +90,8 @@ export interface SearchAlgorithm<C extends Chromosome> {
     setSelectionOperator(selectionOperator: Selection<C>): void;
 
     /**
-     * Sets the local search operators callable by the algorithm under certain circumstances.
-     * @param localSearchOperators the local search operators callable by the algorithm.
+     * Sets the LocalSearch operators callable by the algorithm under certain circumstances.
+     * @param localSearchOperators the LocalSearch operators callable by the algorithm.
      */
     setLocalSearchOperators(localSearchOperators: List<LocalSearch<C>>): void
 
