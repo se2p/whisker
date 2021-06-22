6ba8ac0a645b5058fa9497e96c73bb3b16bd1324
goetz_katharina <goetz_katharina@hotmail.de>
2021-06-22 14:16:56 +0200

    Clean up editor code of example duplicates.

--- whisker-web/src/components/model-editor.js
+++ whisker-web/src/components/model-editor.js
@@ -14,19 +14,13 @@ class ModelEditor {
         this.data = null;
         this.container = document.getElementById("model-editor-canvas");
         this.nodes = [];
-        this.edges = []
-    }
-
-    drawModelEditor() {
-        // todo get new nodes and edges based on the loaded models
-        // todo make a new view for each model -> multiple networks
-        let nodes = [
-            {id: 1, label: "Start", color: "rgb(0,151,163)", title:"test"},
+        this.edges = [];
+        // todo make these empty later on
+        this.dummyNodes = [
+            {id: 1, label: "Start", color: "rgb(0,151,163)", title: "test"},
             {id: 2, label: "End"}
         ];
-
-        // create an array with edges
-        let edges = [
+        this.dummyEdges = [
             {
                 from: 1, to: 2,
                 font: {align: "top"},
@@ -52,86 +46,19 @@ class ModelEditor {
                 },
             }
         ];
-        //Stage.Zeit == 30 && Stage.Punkte == 0 && Bowl.x == 0 &&" +
-        //                     " Bowl.y == -145 & Apple.size == 50 && Bananas.size == 50"
-        //Stage.Zeit <= 30 && Bowl.y == -145 && Apple.size == 50 &&" +
-        //                     " Bananas.size == 50
 
-        this.data = {
-            nodes: nodes,
-            edges: edges,
-        };
-
-        // const editNode = this.editNode;
-        // const clearNodePopUp = this.clearNodePopUp;
-        // const cancelNodeEdit = this.cancelNodeEdit;
-        // const editEdgeWithoutDrag = this.editEdgeWithoutDrag;
+        const addNode = this.addNode;
+        const editNode = this.editNode;
+        const addEdge = this.addEdge;
 
         this.options = {
-            // manipulation: {
-            //     addNode: function (data, callback) {
-            //         console.log("add node test");
-            //     },
-            //     editNode: function (data, callback) {
-            //         console.log("edit node test");
-            //     },
-            //     addEdge: function (data, callback) {
-            //         console.log("add edge test");
-            //         // if (data.from === data.to) {
-            //         //     let r = confirm("Do you want to connect the node to itself?");
-            //         //     if (r !== true) {
-            //         //         callback(null);
-            //         //         return;
-            //         //     }
-            //         // }
-            //         // $("model-edge-operation").innerText = "Add Edge";
-            //         // editEdgeWithoutDrag(data, callback);
-            //     },
-            //     editEdge: function (edgeData, callback) {
-            //         console.log("edit edge test");
-            //         // $("model-edge-operation").innerText = "Edit Edge";
-            //         // editEdgeWithoutDrag(data, callback);
-            //     }
-            // },
-            layout:{randomSeed:2},
+            layout: {randomSeed: 2},
             physics: true,
             manipulation: {
-                addNode: function (data, callback) {
-                    $(ModelEditor.OP_NAME_FIELD).text(i18n.t("model-editor:addNode"));
-                    $(ModelEditor.ID_FIELD).val(data.id);
-                    $(ModelEditor.LABEL_FIELD).val(data.label);
-                    document.getElementById("saveButton").onclick = saveData.bind(
-                        this,
-                        data,
-                        callback
-                    );
-                    document.getElementById("cancelButton").onclick = clearPopUp.bind();
-                },
-                editNode: function (data, callback) {
-                    $(ModelEditor.OP_NAME_FIELD).text(i18n.t("model-editor:editNode"));
-                    $(ModelEditor.ID_FIELD).val(data.id);
-                    $(ModelEditor.LABEL_FIELD).val(data.label);
-
-                    document.getElementById("saveButton").onclick = saveData.bind(
-                        this,
-                        data,
-                        callback
-                    );
-                    document.getElementById("cancelButton").onclick = cancelEdit.bind(
-                        this,
-                        callback
-                    );
-                },
-                addEdge: function (data, callback) {
-                    if (data.from == data.to) {
-                        var r = confirm("Do you want to connect the node to itself?");
-                        if (r == true) {
-                            callback(data);
-                        }
-                    } else {
-                        callback(data);
-                    }
-                },
+                addNode: addNode,
+                editNode: editNode,
+                addEdge: addEdge
+                // todo edit edge?
             },
             locale: $('#lang-select').val(),
             autoResize: true,
@@ -140,23 +67,20 @@ class ModelEditor {
             clickToUse: false
         };
 
-        function clearPopUp() {
-            document.getElementById("saveButton").onclick = null;
-            document.getElementById("cancelButton").onclick = null;
-        //     document.getElementById("model-editor-configuration").style.display = "none";
-        }
+        //Stage.Zeit == 30 && Stage.Punkte == 0 && Bowl.x == 0 &&" +
+        //                     " Bowl.y == -145 & Apple.size == 50 && Bananas.size == 50"
+        //Stage.Zeit <= 30 && Bowl.y == -145 && Apple.size == 50 &&" +
+        //                     " Bananas.size == 50
+        this.loadModel(null);
+        this.setUpButtons();
 
-        function cancelEdit(callback) {
-            clearPopUp();
-            callback(null);
-        }
+    }
 
-        function saveData(data, callback) {
-            data.id = $(ModelEditor.ID_FIELD).val();
-            data.label = $(ModelEditor.LABEL_FIELD).val();
-            callback(data);
-        }
+    // todo get new nodes and edges based on the loaded models
+    // todo make a new view for each model -> multiple networks
 
+    drawModelEditor() {
+        // todo look up how to destroy the old one correctly as in the examples
         this.network = new vis.Network(this.container, this.data, this.options);
         this.network.enableEditMode();
         this.network.on('selectNode', this.network.editNode);
@@ -167,69 +91,117 @@ class ModelEditor {
         });
     }
 
-    editNode(data, cancelAction, callback) {
-        $("model-node-label").value = data.label;
-        $("model-node-saveButton").onclick = this.saveNodeData.bind(
-            this,
-            data,
-            callback
-        );
-        $("model-node-cancelButton").onclick = cancelAction.bind(
-            this,
-            callback
-        );
+    // todo on click on a model tab
+    loadModel(model) {
+
+        if (model == null) {
+            this.data = {nodes: this.dummyNodes, edges: this.dummyEdges};
+        } else {
+            // todo get the nodes
+            // todo get the edges
+
+            let nodes = [];
+            let edges = [];
+            this.data = {nodes: nodes, edges: edges};
+        }
+
+        this.drawModelEditor();
     }
+
+    addNode(data, callback) {
+        $(ModelEditor.OP_NAME_FIELD).text(i18n.t("model-editor:addNode"));
+        $(ModelEditor.ID_FIELD).val(data.id);
+        $(ModelEditor.LABEL_FIELD).val(data.label);
+        document.getElementById("saveButton").onclick = this.saveData;
+        // document.getElementById("cancelButton").onclick = clearPopUp.bind();
+    }
+
+    editNode(data, callback) {
+        $(ModelEditor.OP_NAME_FIELD).text(i18n.t("model-editor:editNode"));
+        $(ModelEditor.ID_FIELD).val(data.id);
+        $(ModelEditor.LABEL_FIELD).val(data.label);
+
+        document.getElementById("saveButton").onclick = this.saveData;
+        // document.getElementById("cancelButton").onclick = cancelEdit.bind(this, callback);
+    }
+
+    saveData(data, callback) {
+        data.id = $(ModelEditor.ID_FIELD).val();
+        data.label = $(ModelEditor.LABEL_FIELD).val();
+        callback(data);
+    }
+
+    addEdge(data, callback) {
+        if (data.from === data.to) {
+            let r = confirm("Do you want to connect the node to itself?");
+            if (r === true) {
+                callback(data);
+            }
+        } else {
+            callback(data);
+        }
+    }
+
+    setUpButtons() {
+        // todo set up button click actions
+    }
+
+    // editNode(data, cancelAction, callback) {
+    //     $("model-node-label").value = data.label;
+    //     $("model-node-saveButton").onclick = this.saveNodeData.bind(
+    //         this,
+    //         data,
+    //         callback
+    //     );
+    //     $("model-node-cancelButton").onclick = cancelAction.bind(
+    //         this,
+    //         callback
+    //     );
+    // }
 
 // Callback passed as parameter is ignored
-    clearNodePopUp() {
-        $("model-node-saveButton").onclick = null;
-        $("model-node-cancelButton").onclick = null;
-    }
+//     clearNodePopUp() {
+//         $("model-node-saveButton").onclick = null;
+//         $("model-node-cancelButton").onclick = null;
+//     }
 
-    cancelNodeEdit(callback) {
-        this.clearNodePopUp();
-        callback(null);
-    }
+    // cancelNodeEdit(callback) {
+    //     this.clearNodePopUp();
+    //     callback(null);
+    // }
 
-    saveNodeData(data, callback) {
-        data.label = $("model-node-label").value;
-        this.clearNodePopUp();
-        callback(data);
-    }
+    // saveNodeData(data, callback) {
+    //     data.label = $("model-node-label").value;
+    //     this.clearNodePopUp();
+    //     callback(data);
+    // }
 
-    editEdgeWithoutDrag(data, callback) {
-        // filling in the popup DOM elements
-        $("model-edge-label").value = data.label;
-        $("model-edge-saveButton").onclick = this.saveEdgeData.bind(
-            this,
-            data,
-            callback
-        );
-        $("model-edge-cancelButton").onclick = this.cancelEdgeEdit.bind(
-            this,
-            callback
-        );
-        $("model-edge-popUp").style.display = "block";
-    }
+    // editEdgeWithoutDrag(data, callback) {
+    //     // filling in the popup DOM elements
+    //     $("model-edge-label").value = data.label;
+    //     $("model-edge-saveButton").onclick = this.saveEdgeData.bind(this, data, callback);
+    //     $("model-edge-cancelButton").onclick = this.cancelEdgeEdit.bind(this, callback);
+    //     $("model-edge-popUp").style.display = "block";
+    // }
 
-    clearEdgePopUp() {
-        $("model-edge-saveButton").onclick = null;
-        $("model-edge-cancelButton").onclick = null;
-        $("model-edge-popUp").style.display = "none";
-    }
+    // clearEdgePopUp() {
+    //     $("model-edge-saveButton").onclick = null;
+    //     $("model-edge-cancelButton").onclick = null;
+    //     $("model-edge-popUp").style.display = "none";
+    // }
 
-    cancelEdgeEdit(callback) {
-        this.clearEdgePopUp();
-        callback(null);
-    }
-
-    saveEdgeData(data, callback) {
-        if (typeof data.to === "object") data.to = data.to.id;
-        if (typeof data.from === "object") data.from = data.from.id;
-        data.label = $("model-edge-label").value;
-        this.clearEdgePopUp();
-        callback(data);
-    }
+    // cancelEdgeEdit(callback) {
+    //     this.clearEdgePopUp();
+    //     callback(null);
+    // }
+    //
+    // saveEdgeData(data, callback) {
+    //     if (typeof data.to === "object") data.to = data.to.id;
+    //     if (typeof data.from === "object") data.from = data.from.id;
+    //     data.label = $("model-edge-label").value;
+    //     this.clearEdgePopUp();
+    //     callback(data);
+    // }
 
 }
 
