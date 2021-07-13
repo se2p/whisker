38eb1aff97ee9e8168faadd096cba45559400f24
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2021-07-13 15:46:55 +0200

    Docker: allow redirection of stdout and stderr

--- whisker-docker.sh
+++ whisker-docker.sh
@@ -1,12 +1,23 @@
 #! /usr/bin/env sh
 
+# Make sure we're actually inside a Docker container before proceeding.
 if [ ! -f /.dockerenv ]; then
-    echo "This script is only supposed to be run within a Docker container!"
-    echo "You cannot run it as a standalone script!"
+    echo "This script is only supposed to be run within a Docker container."
+    echo "You cannot run it as a standalone script."
     exit 1
 fi
 
-node servant/servant.js -d -k -l \
-    -u whisker-web/dist/index.html \
-    -c config/default.json \
-    "$@"
+# The base command for Whisker.
+WHISKER="node servant/servant.js -d -k -l -u whisker-web/dist/index.html"
+
+# Check if redirection of stdout and stderr is desired, and adjust the
+# base command accordingly.
+REDIRECT_OUTPUT="${2}"
+if [ "${REDIRECT_OUTPUT}" = "--" ]; then
+    OUTPUT_DIR="${1}"
+    shift 2
+    WHISKER=">${OUTPUT_DIR}/log-out.txt 2>${OUTPUT_DIR}/log-err.txt ${WHISKER}"
+fi
+
+# Run Whisker with the command line arguments passed to this script.
+eval "${WHISKER} $*"
