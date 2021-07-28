175abd0359201b2befc3175272d18e3093ac863e
UssEva <40119154+UssEva@users.noreply.github.com>
2021-07-28 15:33:20 +0200

    Convert events to strings for recorder

--- whisker-main/src/vm/util.js
+++ whisker-main/src/vm/util.js
@@ -40,6 +40,85 @@ class Util {
     static getScratchKey (vm, keyString) {
         return vm.runtime.ioDevices.keyboard._keyStringToScratchKey(keyString);
     }
+
+    /**
+     * @param {string} target .
+     * @param {number} steps .
+     * @returns {string} .
+     */
+    static clickSprite (target, steps) {
+        return '    t.clickSprite(\''+ target +'\', '+ steps +');';
+    }
+
+    /**
+     * @param {number} x .
+     * @param {number} y .
+     * @param {number} steps .
+     * @returns {string} .
+     */
+    static clickClone (x, y, steps) {
+        return '    t.clickClone('+ x +', '+ y +', '+ steps +');';
+    }
+
+    /**
+     * @param {string} target .
+     * @param {number} x .
+     * @param {number} y .
+     * @returns {string} .
+     */
+    static dragSprite (target, x, y) {
+        return '    t.dragSprite(\''+ target +'\', '+ x +', '+ y +');';
+    }
+
+    /**
+     * @returns {string} .
+     */
+    static clickStage () {
+        return '    t.clickStage();';
+    }
+
+    /**
+     * @param {string} key .
+     * @param {number} steps .
+     * @returns {string} .
+     */
+    static keyPress(key, steps) {
+        return '    t.keyPress(\''+ key +'\', '+ steps +');';
+    }
+
+    /**
+     * @param {boolean} value .
+     * @returns {string} .
+     */
+    static mouseDown (value) {
+        return '    t.mouseDown('+ value +');';
+    }
+
+    /**
+     * @param {number} x .
+     * @param {number} y .
+     * @returns {string} .
+     */
+    static mouseMove (x, y) {
+        return '    t.mouseMove('+ x +', '+ y +');';
+    }
+
+    /**
+     * @param {number} x .
+     * @param {number} y .
+     * @returns {string} .
+     */
+    static mouseMoveToEvent (x, y) {
+        return '    t.mouseMoveToEvent('+ x +', '+ y +');';
+    }
+
+    /**
+     * @param {string} text .
+     * @returns {string} .
+     */
+    static typeText (text) {
+        return '    t.typeText(\''+ text +'\');';
+    }
 }
 
 module.exports = Util;
