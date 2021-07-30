d103a37513e7a17bec5bac0c76a23c565c7c6158
UssEva <40119154+UssEva@users.noreply.github.com>
2021-07-30 16:15:41 +0200

    Update recorded events

--- whisker-web/src/components/input-recorder.js
+++ whisker-web/src/components/input-recorder.js
@@ -3,20 +3,21 @@ const EventEmitter = require('events');
 
 class InputRecorder extends EventEmitter {
 
-    constructor (scratch) {
+    constructor(scratch) {
         super();
 
         this.scratch = scratch;
+        this.vm = this.scratch.vm;
 
-        this.inputs = null;
+        this.events = null;
         this.startTime = null;
 
         this._onInput = this.onInput.bind(this);
 
         this.testBegin = 'const test = async function (t) {\n';
-        this.testEnd = '\n    await t.wait(5000);\n    t.end();\n}';
-        this.export =  '\n\n\n' +
-`module.exports = [
+        this.testEnd = '\n    t.wait(5000);\n    t.end();\n}';
+        this.export = '\n\n\n' +
+            `module.exports = [
     {
         test: test,
         name: 'Recorded Test',
@@ -26,18 +27,18 @@ class InputRecorder extends EventEmitter {
 ];`;
     }
 
-    startRecording () {
+    startRecording() {
         this.emit('startRecording');
-        this.inputs = [];
+        this.events = [];
         this.startTime = Date.now();
         this.scratch.on('input', this._onInput);
     }
 
-    stopRecording () {
+    stopRecording() {
         this.emit('stopRecording');
         this.scratch.removeListener('input', this._onInput);
         this.showInputs();
-        this.inputs = null;
+        this.events = null;
         this.startTime = null;
     }
 
@@ -45,61 +46,52 @@ class InputRecorder extends EventEmitter {
         return this.startTime != null;
     }
 
-    onInput (data) {
+    onInput(data) {
         switch (data.device) {
-        case 'mouse':
-            this.onMouseInput(data);
-            break;
-        case 'keyboard':
-            this.onKeyboardInput(data);
-            break;
-        default:
-            console.error(`Unknown input device: "${data.device}".`);
+            case 'mouse':
+                this.onMouseInput(data);
+                break;
+            case 'keyboard':
+                this.onKeyboardInput(data);
+                break;
+            case 'text':
+                this.onTextInput(data);
+                break;
+            default:
+                console.error(`Unknown input device: "${data.device}".`);
         }
     }
 
-    onMouseInput (data) {
-        const coords = Util.getScratchCoords(this.scratch.vm, data.x, data.y);
+    onMouseInput(data) {
+        const steps = Date.now() - this.startTime;
+        let event;
 
-        const input = {
-            device: 'mouse',
-            x: coords.x.toFixed(2),
-            y: coords.y.toFixed(2)
-        };
-
-        if ('isDown' in data) {
-            input.isDown = data.isDown;
+        if (data.isDown) {
+            // TODO: How to distinguish between sprite and stage?
+            event = Util.clickClone(data.x, data.y, steps);
         } else {
-            // TODO: How will mouse movement be handled?
-            return;
+            // TODO: How to handle mouse movement?
+            event = Util.mouseMove(data.x, data.y);
         }
-
-        this.recordInput(input);
+        this.events.push(event);
     }
 
-    onKeyboardInput (data) {
-        const key = Util.getScratchKey(this.scratch.vm, data.key);
-
-        const input = {
-            device: 'keyboard',
-            key,
-            isDown: data.isDown
-        };
-
-        this.recordInput(input);
+    onKeyboardInput(data) {
+        if (data.isDown) {
+            const key = Util.getScratchKey(this.vm, data.key);
+            const steps = Date.now() - this.startTime;
+            const event = Util.keyPress(key, steps);
+            this.events.push(event);
+        }
     }
 
-    recordInput (input) {
-        this.inputs.push({
-            time: Date.now() - this.startTime,
-            input
-        });
+    onTextInput(data) {
+        const event = Util.typeText(data.answer);
+        this.events.push(event);
     }
 
-    // TODO Convert input to event functions
-    showInputs () {
-        const inputs = this.inputs.map(input => `        ${JSON.stringify(input)}`);
-        let inputCode = `    t.addInputs([\n${inputs.join(',\n')}\n    ]);`;
+    showInputs() {
+        let inputCode = `${this.events.join('\n')}`;
         Whisker.testEditor.setValue(this.testBegin + inputCode + this.testEnd + this.export);
         location.href = "#"; // this line is required to work around a bug in WebKit (Chrome / Safari) according to stackoverflow
         location.href = '#test-editor'
