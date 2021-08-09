9f2240dcf5213e89848c8124cdc5aac6f5e4f908
Patric Feldmeier <patric.feldmeier@uni-passau.de>
2021-08-09 13:43:26 +0200

    Fix DynamicScratchEventExtractor; we always need at least one WaitEvent

--- whisker-main/src/whisker/testcase/DynamicScratchEventExtractor.ts
+++ whisker-main/src/whisker/testcase/DynamicScratchEventExtractor.ts
@@ -64,11 +64,12 @@ export class DynamicScratchEventExtractor extends ScratchEventExtractor {
             }
         }
 
-        eventList.add(new WaitEvent())
         if(eventList.getElements().some(event => event instanceof TypeTextEvent)){
             eventList = eventList.filter(event => event instanceof TypeTextEvent);
         }
 
+        // We always need a WaitEvent otherwise, ExtensionLocalSearch if applied will produce codons having values of -1
+        eventList.add(new WaitEvent())
         return eventList.distinctObjects();
     }
 }
