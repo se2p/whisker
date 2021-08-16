07056bb8ae694c8df2d9f28811c57d8aca6c773d
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2021-08-16 20:40:08 +0200

    Update Dockerfiles and add documentation

--- servant/whisker-docker.sh
+++ servant/whisker-docker.sh
@@ -7,30 +7,21 @@ if [ ! -f /.dockerenv ]; then
     exit 1
 fi
 
-# The base command for Whisker.
+# The base command for Whisker. We enable headless mode, console and log
+# forwarding, and already set the file URL of the Whisker instance.
 WHISKER="node servant.js -d -k -l -u ../whisker-web/dist/index.html"
 
-# Check if copying of artefacts to the host and redirection of stdout and stderr
-# are desired, and adjust the base command accordingly.
+# Check if redirection of stdout and stderr to files are desired, and adjust
+# the base command accordingly.
 REDIRECT_OUTPUT="${2}"
 if [ "${REDIRECT_OUTPUT}" = "--" ]; then
     OUTPUT_DIR="${1}"
     shift 2
-    exec ${WHISKER} "$@" \
-        >"${OUTPUT_DIR}/log-out.txt" \
-        2>"${OUTPUT_DIR}/log-err.txt" \
-        && [ -f "tests.js" ] && cp "tests.js" "${OUTPUT_DIR}"
-else
-    exec ${WHISKER} "$@"
+    WHISKER=>"${OUTPUT_DIR}/log-out.txt" 2>"${OUTPUT_DIR}/log-err.txt" ${WHISKER}
 fi
 
-# Run Whisker with the command line arguments passed to this script.
+# Finally, run Whisker with the command line arguments passed to this script.
+# Make sure to use `exec` here (instead of `eval`). This allows Whisker to receive
+# any Unix signals sent to this wrapper script.
 # https://docs.docker.com/develop/develop-images/dockerfile_best-practices/#entrypoint
-# https://snyk.io/blog/10-best-practices-to-containerize-nodejs-web-applications-with-docker/
-# https://github.com/nodejs/docker-node/blob/main/docs/BestPractices.md#handling-kernel-signals
-# https://github.com/Yelp/dumb-init
-# Make sure to use `exec` here (instead of `eval`) such that the following command becomes the
-# container's PID 1. This allows Whisker to receive any Unix signals sent to the Docker container.
-# However, Node.js was not designed to run as PID 1. So we need to use an init wrapper that
-# forwards all received signals to the Node.js child process. (We use `dumb-init`).
-
+exec ${WHISKER} "$@"
