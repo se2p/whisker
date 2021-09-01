7f6e66cbc72d2e37097651a2b167b341622b49e2
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2021-09-01 22:36:31 +0200

    Build docker image on HDD instead of SSD (InfoSun office workstations)
    Building on the HDD instead of the SSD takes just a handful of more seconds.
    But we need not worry about free disk space as much.

--- docker-build.sh
+++ docker-build.sh
@@ -8,7 +8,7 @@ readonly TAG="whisker:${BRANCH}-${COMMIT}"
 function set_docker_cmd() {
     if [[ -z "${DOCKER_CMD}" ]]; then
         if command -v dockerd-rootless-infosun &>/dev/null; then
-            DOCKER_CMD="dockerd-rootless-infosun --data-root /local/${USER}/docker -- docker"
+            DOCKER_CMD="dockerd-rootless-infosun --data-root /local/hdd/${USER}/docker -- docker"
         else
             DOCKER_CMD="docker"
         fi
