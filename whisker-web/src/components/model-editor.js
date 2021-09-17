dcaea5670b6c9ec9c828634d7e5bc23b55c020a6
Katharina Götz <goetz_katharina@hotmail.de>
2021-09-17 12:20:28 +0200

    Model general settings in editor.

--- whisker-web/src/components/model-editor.js
+++ whisker-web/src/components/model-editor.js
@@ -16,6 +16,7 @@ class ModelEditor {
     static MODEL_ID_FIELD = '#model-id';
     static PROGRAM_TYPE_CHOICE = '#model-type-program';
     static USER_TYPE_CHOICE = '#model-type-user';
+    static END_TYPE_CHOICE = '#model-type-end';
 
     // below the model editor
     static SAVE_BUTTON = '#model-editor-save';
@@ -149,7 +150,6 @@ class ModelEditor {
         // document.getElementsByClassName('vis-close')[0].val("Löschen");
     }
 
-    // todo on click on a model tab
     loadModel(tabNbr = 0) {
         this.currentTab = tabNbr;
 
@@ -181,9 +181,15 @@ class ModelEditor {
         if (this.models[tabNbr].usage === "program" || this.models[tabNbr].usage === undefined) {
             $(ModelEditor.PROGRAM_TYPE_CHOICE).prop('checked', true);
             $(ModelEditor.USER_TYPE_CHOICE).prop('checked', false);
-        } else {
+            $(ModelEditor.END_TYPE_CHOICE).prop('checked', false);
+        } else if (this.models[tabNbr].usage === "user") {
             $(ModelEditor.PROGRAM_TYPE_CHOICE).prop('checked', false);
             $(ModelEditor.USER_TYPE_CHOICE).prop('checked', true);
+            $(ModelEditor.END_TYPE_CHOICE).prop('checked', false);
+        } else {
+            $(ModelEditor.PROGRAM_TYPE_CHOICE).prop('checked', false);
+            $(ModelEditor.USER_TYPE_CHOICE).prop('checked', false);
+            $(ModelEditor.END_TYPE_CHOICE).prop('checked', true);
         }
     }
 
@@ -250,20 +256,9 @@ class ModelEditor {
      * Set up gui buttons such as save, apply, add tab etc.
      */
     setUpButtons() {
-        // apply
-        $(ModelEditor.APPLY_BUTTON).on('click', () => {
-            let json = JSON.stringify(this.models, null, 4);
-            this.modelTester.load(json);
-            $('#model-label').html('Loaded from model editor');
-            // $('#modelfile') todo maybe delete the fileselects content with clear
-        })
-
-        // download
-        $(ModelEditor.SAVE_BUTTON).on('click', () => {
-            let json = JSON.stringify(this.modelTester.getAllModels(), null, 4);
-            const blob = new Blob([json], {type: 'text/plain;charset=utf-8'});
-            FileSaver.saveAs(blob, 'models.json');
-        });
+        // apply and download below model editor
+        $(ModelEditor.APPLY_BUTTON).on('click', this.applyButton.bind(this));
+        $(ModelEditor.SAVE_BUTTON).on('click', this.downloadButton.bind(this));
 
         // tab behaviour
         $(ModelEditor.ADD_TAB).on('click', () => {
@@ -273,18 +268,17 @@ class ModelEditor {
         })
 
         // General settings of model
-        $(ModelEditor.MODEL_ID_FIELD).on('keyup change', () => {
-            this.models[this.currentTab].id = $(ModelEditor.MODEL_ID_FIELD).value;
-        })
+        $(ModelEditor.MODEL_ID_FIELD).on('keyup change', this.onModelIDChange.bind(this));
         $(ModelEditor.PROGRAM_TYPE_CHOICE).on('click', () => {
             this.models[this.currentTab].usage = "program";
         })
         $(ModelEditor.USER_TYPE_CHOICE).on('click', () => {
             this.models[this.currentTab].usage = "user";
         })
+        $(ModelEditor.END_TYPE_CHOICE).on('click', () => {
+            this.models[this.currentTab].usage = "end";
+        })
     }
-
-    // todo dont allow two identical model ids
 
     /**
      * Change the view to a model based on a clicked tab.
@@ -337,6 +331,60 @@ class ModelEditor {
             this.changeToTab(button.value);
         }
         $(ModelEditor.TABS).append(button);
+    }
+
+    applyButton() {
+        // get current active tab
+        let lastFocus = $(ModelEditor.TABS).children('.active')[0].textContent;
+
+        // load the models into the modelTester
+        let json = JSON.stringify(this.models, null, 4);
+        this.modelTester.load(json);
+        $('#model-label').html('Loaded from model editor');
+
+        // change to last active tab
+        let lastIndex = 0;
+        let newChildren = $(ModelEditor.TABS).children();
+        for (let i = 0; i < newChildren.length; i++) {
+            if (newChildren[i].textContent === lastFocus) {
+                lastIndex = newChildren[i].value;
+                break;
+            }
+        }
+
+        this.changeToTab(lastIndex);
+    }
+
+    downloadButton() {
+        let json = JSON.stringify(this.modelTester.getAllModels(), null, 4);
+        const blob = new Blob([json], {type: 'text/plain;charset=utf-8'});
+        FileSaver.saveAs(blob, 'models.json');
+    }
+
+    onModelIDChange() {
+        let newValue = $(ModelEditor.MODEL_ID_FIELD).val();
+
+        // ignore all not changing buttons such as SHIFT
+        if (newValue === $(ModelEditor.TABS).children('.active')[0].textContent) {
+            return;
+        }
+
+        let hasThisId = 0;
+        for (let i = 0; i < this.models.length; i++) {
+            if (this.models[i].id === newValue) {
+                hasThisId++;
+            }
+        }
+
+        if (hasThisId > 0) {
+            this.models[this.currentTab].id = newValue + hasThisId;
+            setTimeout(() => {
+                $(ModelEditor.MODEL_ID_FIELD).val(this.models[this.currentTab].id);
+            }, 1000)
+        } else {
+            this.models[this.currentTab].id = newValue;
+        }
+        $(ModelEditor.TABS).children('.active')[0].textContent = this.models[this.currentTab].id;
     }
 
     // editNode(data, cancelAction, callback) {
