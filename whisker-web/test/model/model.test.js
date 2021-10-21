7d6bebd4025653443bc4e8b11c2d85bf5e8aadf3
Katharina Götz <goetz_katharina@hotmail.de>
2021-10-21 14:04:08 +0200

    Fixed model test again, needed an int.

--- whisker-web/test/model/model.test.js
+++ whisker-web/test/model/model.test.js
@@ -137,17 +137,18 @@ describe('Model tests on multiple events per step', () => {
         await expect(modelCoverage).toBe("1.00");
     }, timeout);
 
-    jest.setTimeout(80000);
+    jest.setTimeout(60000);
     test('fruitcatcher with random model input', async () => {
         await loadProject('test/model/scratch-programs/fruitcatcher.sb3',
             'test/model/model-jsons/fruitcatcher-random-fruit.json');
         await page.evaluate(factor => document.querySelector('#model-duration').value = factor, 33);
-        await page.evaluate(factor => document.querySelector('#model-repetitions').value = factor, 10);
+        await page.evaluate(factor => document.querySelector('#model-repetitions').value = factor, 8);
 
         await (await page.$('#run-all-tests')).click();
         let {errorsInModel, failsInModel, modelCoverage} = await readModelErrors();
         await expect(errorsInModel).toBe("0");
         await expect(failsInModel).toBe("0");
-        await expect(modelCoverage).toBeGreaterThan(0.9);
+        // as there are not enough repetitions (for shorter pipeline) only test for coverage > 0.9.
+        await expect(Number.parseInt(modelCoverage)).toBeGreaterThan(0.9);
     })
 });
