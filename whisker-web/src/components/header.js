a313787d2d3ceac9700442c3cbdb911b6982167b
UssEva <evagruendinger@web.de>
2021-10-28 18:57:00 +0200

    Update header component

--- whisker-web/src/components/header.js
+++ whisker-web/src/components/header.js
@@ -2,6 +2,17 @@ const header = `<div id="scroll-background">
             <a id="link">
                 <img id="small-logo" class="hide" alt="Whisker Icon" src=""/>
             </a>
+            <!-- Navigation -->
+            <!-- Currently disabled as it is the only navigation item as long as faq/demo is disabled. Logo has the functionality now.
+                <ul id="nav-links" class="nav justify-content-center">
+                    <li class="nav-item">
+                        <a id="link" class="nav-link active" href="index.html" data-i18n="project-link"></a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="faq" class="nav-link" href="./faq.html" data-i18n="faq"></a>
+                    </li>
+                </ul>
+            -->
             <!-- Toggles -->
             <div class="btn-group-toggle">
                 <!-- Tap output toggle -->
@@ -12,7 +23,7 @@ const header = `<div id="scroll-background">
                         <span class="slider round"></span>
                     </label>
                 </span>
-                <span data-i18n="header:tap"></span>
+                <span data-i18n="header:tap" class="toggleDescription"></span>
                 <!-- Log toggle -->
                 <span class="toggleWrapper">
                 <label class="switch" data-i18n="[title]header:t-log"
@@ -20,8 +31,8 @@ const header = `<div id="scroll-background">
                     <input id="toggle-log" class="checkbox-button" type="checkbox" autocomplete="off">
                     <span class="slider round"></span>
                 </label>
-            </span>
-                <span data-i18n="header:log"></span>
+                </span>
+                <span data-i18n="header:log" class="toggleDescription"></span>
                 <!-- Test generation toggle -->
                 <span class="toggleWrapper">
                     <label class="switch" data-i18n="[title]header:show-advanced"
@@ -30,7 +41,23 @@ const header = `<div id="scroll-background">
                         <span class="slider round"></span>
                     </label>
                 </span>
-                <span data-i18n="header:advanced"></span>
+                <span data-i18n="header:advanced" class="toggleDescription"></span>
+                <!-- Test editor toggle -->
+                <span class="toggleWrapper">
+                     <label class="switch">
+                         <input id="toggle-test-editor" class="checkbox-button" type="checkbox" autocomplete="off">
+                         <span class="slider round"></span>
+                     </label>
+                </span>
+                <span data-i18n="test-editor" class="toggleDescription"></span>
+                <!-- Model editor toggle -->
+                <span class="toggleWrapper">
+                     <label class="switch">
+                         <input id="toggle-model-editor" class="checkbox-button" type="checkbox" autocomplete="off">
+                         <span class="slider round"></span>
+                     </label>
+                </span>
+                <span data-i18n="model-editor" class="toggleDescription"></span>
             </div>
             <!-- Language selector -->
             <div class="links">
