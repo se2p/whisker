acb8a00932957238ad6ee2170426553e9d7d8428
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2022-02-17 12:30:05 +0100

    Remove abandoned integration tests

--- whisker-web/test/integration/searchTargets/clickInOrder/clickSingleTarget.sh
+++
@@ -1,8 +0,0 @@
-SCRATCH_FILE=ClickInOrder.sb3
-SCRATCH_PATH="$(cd "$(dirname "$SCRATCH_FILE")"; pwd -P)/$(basename "$SCRATCH_FILE")"
-
-CONFIG_FILE=../default_say.json
-CONFIG_PATH="$(cd "$(dirname "$CONFIG_FILE")"; pwd -P)/$(basename "$CONFIG_FILE")"
-
-cd ../../../../servant
-node servant.js -d -g -s $SCRATCH_PATH -c $CONFIG_PATH -k
