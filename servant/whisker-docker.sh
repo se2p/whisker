27884ab5286008ddc6354895cccbcd0ed981129c
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2022-02-21 18:35:12 +0100

    Fix error in shell script

--- servant/whisker-docker.sh
+++ servant/whisker-docker.sh
@@ -10,8 +10,12 @@ fi
 # The base command for Whisker. We enable headless mode, console and log
 # forwarding, and already set the file URL of the Whisker instance.
 whisker() {
-    info
-    node servant.js \
+    print_info >&2
+
+    # Make sure to use `exec` here (instead of `eval`). This allows Whisker to receive any
+    # Unix signals sent to this wrapper script. See:
+    # https://docs.docker.com/develop/develop-images/dockerfile_best-practices/#entrypoint
+    exec node servant.js \
         -d \
         -k \
         -l \
@@ -19,10 +23,10 @@ whisker() {
         "$@"
 }
 
-info() {
+print_info() {
     node_ver=$(node --version)
     . /etc/os-release
-    echo "INFO: Running ${node_ver} on ${PRETTY_NAME}" >&2
+    echo "INFO: Running Node.js ${node_ver} on ${PRETTY_NAME}"
 }
 
 # We support redirection of stdout and stderr to files in a custom directory.
@@ -30,21 +34,19 @@ info() {
 # by `--`, followed by the arguments intended for Whisker. We first check if
 # redirection of stdout and stderr to files is desired, and adjust the `whisker`
 # base command accordingly. Finally, we run Whisker with the command line arguments
-# passed to this script. Make sure to use `exec` here (instead of `eval`). This
-# allows Whisker to receive any Unix signals sent to this wrapper script. See:
-# https://docs.docker.com/develop/develop-images/dockerfile_best-practices/#entrypoint
+# passed to this script.
 REDIRECT_OUTPUT="${2}"
 if [ "${REDIRECT_OUTPUT}" = "--" ]; then
     # Redirection is desired. Script invocation syntax:
-    #   ./whisker-docker.sh <output-dir> -- <whisker-args>
+    #   whisker-docker.sh <output-dir> -- <whisker-args>
     OUTPUT_DIR="${1}"
     shift 2
-    exec whisker "$@" \
-        >"${OUTPUT_DIR}/log-out.txt" \
-        2>"${OUTPUT_DIR}/log-err.txt"
+    whisker "$@" \
+        >"${OUTPUT_DIR}/whisker-log-out.txt" \
+        2>"${OUTPUT_DIR}/whisker-log-err.txt"
 else
     # No redirection. Script invocation syntax:
-    #   ./whisker-docker.sh <whisker-args>
-    exec whisker "$@"
+    #   whisker-docker.sh <whisker-args>
+    whisker "$@"
 fi
 
