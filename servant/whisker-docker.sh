54c33de1faa04bbb0cb5703837dc8d9fbbb13585
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2022-02-21 18:08:14 +0100

    Update docker-related shell scripts

--- servant/whisker-docker.sh
+++ servant/whisker-docker.sh
@@ -9,22 +9,35 @@ fi
 
 # The base command for Whisker. We enable headless mode, console and log
 # forwarding, and already set the file URL of the Whisker instance.
-WHISKER="node servant.js -d -k -l -u ../whisker-web/dist/index.html"
+whisker() {
+    node servant.js \
+        -d \
+        -k \
+        -l \
+        -u ../whisker-web/dist/index.html \
+        "$@"
+}
 
-# Check if redirection of stdout and stderr to files are desired, and adjust
-# the base command accordingly.
-# Finally, run Whisker with the command line arguments passed to this script.
-# Make sure to use `exec` here (instead of `eval`). This allows Whisker to 
-# receive any Unix signals sent to this wrapper script.
+# We support redirection of stdout and stderr to files in a custom directory.
+# This directory must be specified as first argument of this script, followed
+# by `--`, followed by the arguments intended for Whisker. We first check if
+# redirection of stdout and stderr to files is desired, and adjust the `whisker`
+# base command accordingly. Finally, we run Whisker with the command line arguments
+# passed to this script. Make sure to use `exec` here (instead of `eval`). This
+# allows Whisker to receive any Unix signals sent to this wrapper script. See:
 # https://docs.docker.com/develop/develop-images/dockerfile_best-practices/#entrypoint
 REDIRECT_OUTPUT="${2}"
 if [ "${REDIRECT_OUTPUT}" = "--" ]; then
+    # Redirection is desired. Script invocation syntax:
+    #   ./whisker-docker.sh <output-dir> -- <whisker-args>
     OUTPUT_DIR="${1}"
     shift 2
-    exec ${WHISKER} "$@" \
+    exec whisker "$@" \
         >"${OUTPUT_DIR}/log-out.txt" \
         2>"${OUTPUT_DIR}/log-err.txt"
 else
-    exec ${WHISKER} "$@"
+    # No redirection. Script invocation syntax:
+    #   ./whisker-docker.sh <whisker-args>
+    exec whisker "$@"
 fi
 
