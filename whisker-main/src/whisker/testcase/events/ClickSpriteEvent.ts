daa2a6ccdd2fcd2eae19b122089f08bc46e2a8f6
Patric Feldmeier <patric.feldmeier@uni-passau.de>
2022-02-26 18:42:15 +0100

    Fix tests

--- whisker-main/src/whisker/testcase/events/ClickSpriteEvent.ts
+++ whisker-main/src/whisker/testcase/events/ClickSpriteEvent.ts
@@ -82,7 +82,17 @@ export class ClickSpriteEvent extends ScratchEvent {
     }
 
     stringIdentifier(): string {
-        // TODO: Find a good way to deal with clones....
-        return `ClickSpriteEvent-${this._target.sprite.name}`;
+        if (this._target.isOriginal || Container.isNeuroevolution) {
+            return `ClickSpriteEvent-${this._target.sprite.name}`;
+        } else {
+            // The stringIdentifier of ClickSpriteEvents having to click at a clone represents a special case
+            // since neither are the x and y coordinates of the Clone determined within the EventExtraction, nor are
+            // they specified during the search. In the case of having two clones located at exactly the same position,
+            // we only include one event for both clones since even if we would add two separate events, the effect
+            // of both would be the same, namely clicking at the specified location. Furthermore, as soon as both
+            // clones move away from each other, the coordinates change and we add separate events for both of
+            // them.
+            return `ClickClone-${this._target.sprite.name}-${this._target.x}-${this._target.y}`;
+        }
     }
 }
