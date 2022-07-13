a3622d0f8967b2c50a10b6bbb08764d5566874a8
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2022-07-13 10:13:21 +0200

    Avoid closing the browser

--- servant/servant.js
+++ servant/servant.js
@@ -1,75 +1,16 @@
-const puppeteer = require("puppeteer");
 const logger = require("./logger");
-const {subcommand, opts: {consoleForwarded, headless, whiskerUrl}} = require('./cli');
+const {subcommand} = require('./cli');
 const {resolve} = require('path');
-
-async function openNewBrowser() {
-    const args = [
-        '--disable-gpu',
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        // '--use-gl=desktop', // could be used next to headless, but pages tend to quit unexpectedly
-    ];
-
-    return await puppeteer.launch({
-        headless,
-        args,
-        devtools: process.env.NODE_ENV !== "production",
-
-        // If specified, use the given version of Chromium/Chrome instead of the one bundled with Puppeteer.
-        // Note: Puppeteer is only guaranteed to work with the bundled Chromium, use at own risk.
-        // https://github.com/puppeteer/puppeteer/blob/v10.2.0/docs/api.md#puppeteerlaunchoptions
-        // https://github.com/puppeteer/puppeteer/blob/v10.2.0/docs/api.md#environment-variables
-        // https://github.com/puppeteer/puppeteer/issues/1793#issuecomment-358216238
-        executablePath: process.env.CHROME_BIN || null,
-    });
-}
-
-async function openNewPage(browser) {
-    const page = await browser.newPage({context: Date.now()});
-    page.on('error', (error) => {
-        logger.error(error);
-        return Promise.reject(error);
-    }).on('pageerror', (error) => {
-        logger.error(error);
-        return Promise.reject(error);
-    });
-
-    if (consoleForwarded) {
-        // https://github.com/puppeteer/puppeteer/issues/1512#issuecomment-349784408
-        // https://github.com/puppeteer/puppeteer/blob/main/docs/api.md#class-consolemessage
-        page.on('console', msg => {
-            if (msg.type() === "warning") {
-                logger.warn(`Forwarded: ${msg.text()}`);
-            } else {
-                logger.info(`Forwarded: ${msg.text()}`);
-            }
-        });
-    }
-
-    return page;
-}
+const {openNewBrowser, openNewPage} = require("./whisker-web");
 
 void async function main() {
-    let browser = null;
-
-    try {
-        browser = await openNewBrowser();
-    } catch (e) {
-        logger.error(e);
-        return Promise.reject(e);
-    }
-
     // The convention is to put the code for a Whisker subcommand "cmd" into a JavaScript module "cmd.js".
     // The module must only export a function accepting the "openNewPage" callback.
-    const runSubcommand = require(resolve(subcommand));
-
     try {
-        return await runSubcommand(openNewPage.bind(null, browser));
+        const browser = await openNewBrowser();
+        return await require(resolve(subcommand))(openNewPage.bind(null, browser));
     } catch (e) {
         logger.error(e);
         return Promise.reject(e);
-    } finally {
-        await browser.close()
     }
 }();
