573efb001d419b52cfdffa764fe0bc1a5ddae0f5
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2022-07-13 12:59:36 +0200

    Close the browser globally

--- servant/servant.js
+++ servant/servant.js
@@ -6,11 +6,16 @@ const {openNewBrowser, openNewPage} = require("./whisker-web");
 void async function main() {
     // The convention is to put the code for a Whisker subcommand "cmd" into a JavaScript module "cmd.js".
     // The module must only export a function accepting the "openNewPage" callback.
+    let browser = null;
     try {
-        const browser = await openNewBrowser();
+        browser = await openNewBrowser();
         return await require(resolve(subcommand))(openNewPage.bind(null, browser));
     } catch (e) {
         logger.error(e);
         return Promise.reject(e);
+    } finally {
+        if (browser) {
+            await browser.close();
+        }
     }
 }();
