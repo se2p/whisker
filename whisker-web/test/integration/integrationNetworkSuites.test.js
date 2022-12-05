887bcfcc9f7945025b680156519da69614ec9358
Patric Feldmeier <feldmeie@fim.uni-passau.de>
2022-12-05 07:43:06 +0100

    Increase timeout for networkSuites integrationTests

--- whisker-web/test/integration/integrationNetworkSuites.test.js
+++ whisker-web/test/integration/integrationNetworkSuites.test.js
@@ -3,7 +3,7 @@ const fileUrl = require('file-url');
 // FIXME: this global variable is actually defined in jest.config.js, but for some reason it is "undefined" here.
 const URL = "dist/index.html";
 
-const timeout = process.env.SLOWMO ? 100000 : 90000;
+const timeout = process.env.SLOWMO ? 150000 : 140000;
 const ACCELERATION = 10;
 
 async function loadProject(scratchPath) {
