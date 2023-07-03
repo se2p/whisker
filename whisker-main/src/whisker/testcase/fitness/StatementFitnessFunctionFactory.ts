a77a653597bc2f73596990492e842f80aac700e2
Patric Feldmeier <feldmeie@fim.uni-passau.de>
2023-07-03 18:11:29 +0200

    Fix tests

--- whisker-main/src/whisker/testcase/fitness/StatementFitnessFunctionFactory.ts
+++ whisker-main/src/whisker/testcase/fitness/StatementFitnessFunctionFactory.ts
@@ -7,10 +7,10 @@ export class StatementFitnessFunctionFactory {
 
     extractFitnessFunctions(vm: VirtualMachine, targets: string[]): StatementFitnessFunction[] {
         const fitnessFunctions: StatementFitnessFunction[] = [];
-        Container.cfg = generateCFG(vm);
-        Container.cdg = generateCDG(Container.cfg);
 
         if (!(vm === undefined || vm === null)) {
+            Container.cfg = generateCFG(vm);
+            Container.cdg = generateCDG(Container.cfg);
             for (const node of Container.cdg.getAllNodes()) {
                 // TODO: Do we need fitness functions for entry and exit
                 if (node.id == "Entry" || node.id == "Exit" || node.id == "start") {
