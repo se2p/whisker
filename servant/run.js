e2e482a3d5d3d37b0da9abd56f896d5260da96af
Patric Feldmeier <feldmeie@fim.uni-passau.de>
2023-11-13 09:58:20 +0100

    Fix bug where csv got trimmed in static mutation analysis runs

--- servant/run.js
+++ servant/run.js
@@ -15,7 +15,13 @@ async function run(openNewPage) {
 
     if (csvFile) {
         console.info(`Creating CSV summary in ${csvFile}`);
-        fs.writeFileSync(csvFile, removeDuplicateHeaders(csvs).join(os.EOL));
+
+        // There can only be multiple headers if there are more than one csv result.
+        if (csvs.length > 1) {
+            fs.writeFileSync(csvFile, removeDuplicateHeaders(csvs).join(os.EOL));
+        } else {
+            fs.writeFileSync(csvFile, csvs.toString());
+        }
     }
 }
 
