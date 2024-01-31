2f60fef5a389a63f69b373fb199fdc687a20f3b7
Patric Feldmeier <feldmeie@fim.uni-passau.de>
2024-01-31 18:06:52 +0100

    Remove unnecessary eventSelector and eventExtractor in observer execution

--- whisker-main/src/whisker/testgenerator/AssertionGenerator.ts
+++ whisker-main/src/whisker/testgenerator/AssertionGenerator.ts
@@ -102,7 +102,7 @@ export class AssertionGenerator {
 
 
     private async _executeWithObserver(test: WhiskerTest)  {
-        const executor = new TestExecutor(Container.vmWrapper, Container.config.getEventExtractor(), Container.config.getEventSelector());
+        const executor = new TestExecutor(Container.vmWrapper, undefined, undefined);
         const observer = new AssertionObserver();
         executor.attach(observer);
         const coverageGroundTruth = test.chromosome.coverage;
