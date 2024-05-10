5b6020260496b03e4903cf820f0b38a17a6e9b8a
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2024-05-10 09:33:29 +0200

    feat: Apply fix for #217 only when save states are used

--- whisker-web/src/index.js
+++ whisker-web/src/index.js
@@ -215,10 +215,13 @@ const _runTestsWithCoverage = async function (vm, project, tests) {
 
         let mutantPrograms = [];
         try {
-            // Loading the project again seems unnecessary here. But removing
-            // this line can cause occasional crashes in the renderer when
-            // restoring the save state between test executions. See issue #217.
-            await vm.loadProject(project);
+            if (props.useSaveStates) {
+                // Loading the project again seems unnecessary here. But removing
+                // this line can cause occasional crashes in the renderer when
+                // restoring the save state between test executions. See issue #217.
+                await vm.loadProject(project);
+            }
+
             vm.runtime.onBlockCovered(blockId => CoverageGenerator._coverBlock(blockId));
 
             CoverageGenerator.prepareVM(vm);
