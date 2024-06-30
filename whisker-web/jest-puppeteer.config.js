e0b16d5bfaee7ca8dc685ddd5aae15455fb9fb78
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2024-06-30 10:42:41 +0200

    fix(integration-tests): fix timeouts
    The use of "devTools: true" appears to have prevented Chromium from
    terminating properly, which resulted in the integration tests running
    into the 20 second timeout.

--- whisker-web/jest-puppeteer.config.js
+++ whisker-web/jest-puppeteer.config.js
@@ -2,6 +2,6 @@ module.exports = {
     launch: {
         headless: process.env.HEADLESS !== 'false',
         slowMo: process.env.SLOWMO ? process.env.SLOWMO : 0,
-        devtools: true
+        devtools: false
     }
 }
