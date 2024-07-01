fdbb043db8ccdc4bd6a9be53fb8839f6bdefa7fc
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2024-07-01 08:13:01 +0200

    fix(CI): run integration tests with --no-sandbox
    It seems when we run the docker container of our CI/CD image, we are the
    root user. Then, chromium refuses to start.

--- whisker-web/jest-puppeteer.config.js
+++ whisker-web/jest-puppeteer.config.js
@@ -2,6 +2,9 @@ module.exports = {
     launch: {
         headless: process.env.HEADLESS !== 'false',
         slowMo: process.env.SLOWMO ? process.env.SLOWMO : 0,
-        devtools: false
+        devtools: false,
+        args: [
+            '--no-sandbox',
+        ],
     }
 }
