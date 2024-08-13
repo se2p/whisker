6bb84e73ef8d4c0801d6f02d26d8ecf4e0255330
Dani Lipp <lipp13@ads.uni-passau.de>
2024-08-13 21:28:35 +0200

    an error during loading models in ModelTester now clears the loaded models

--- whisker-main/test/whisker/model/ModelTester.test.ts
+++ whisker-main/test/whisker/model/ModelTester.test.ts
@@ -11,18 +11,6 @@ describe('ModelTester', () => {
         expect(modelTester.programModelsLoaded()).toBe(true);
         expect(modelTester.userModelsLoaded()).toBe(false);
         expect(modelTester.someModelLoaded()).toBe(true);
-        modelTester = new ModelTester();
-        /*
-        if the previous line is not added then the test will fail, since a model is still loaded from the previous test
-        case within this method. If the loaded models should be removed in an error case, then the catch clause of the
-        `modelTester.load(...)` method must be changed to set to current loaded models to [].
-        Otherwise, the method is correct and this test is buggy and should be split into separate tests or fixed.
-        I suggest this is again a buggy test which did not catch her attention, because the assertions did not
-        actually assert anything due to the missing matcher after the call to `expect(...)`.
-        This test case be either split into multiple tests or the modelTester instance be replaced by a fresh
-        instance to clear the already loaded models. Many tests in the "../../../src/whisker/model" directory are
-        rather big, so not splitting the test would match the current style of the other tests.
-        */
         expect(() => {
             modelTester.load(faultyModel);
         }).toThrow();
