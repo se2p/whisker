96befd78ada89ae9d540c4970ff23a0d02af6794
Dani Lipp <lipp13@ads.uni-passau.de>
2024-09-04 15:21:55 +0200

    refactored tests for ModelLoader.test.ts

--- whisker-main/test/whisker/model/util/ModelLoader.test.ts
+++ whisker-main/test/whisker/model/util/ModelLoader.test.ts
@@ -5,103 +5,42 @@ import {ModelLoader} from "../../../../src/whisker/model/util/ModelLoader";
  * Test for errors for the moment
  */
 describe('ModelLoaderjson', () => {
-    test('Load model from json', () => {
-        const text = readFileSync('test/whisker/model/util/SimpleGraph.json', 'utf8');
-        const loader = new ModelLoader();
-        expect(loader.loadModels(text)).not.toBeNull();
+    describe('loadModels() returns correct number of models', () => {
+        const table: [string, string, number, number, number][] = [
+            ["Load model from json", 'test/whisker/model/util/SimpleGraph.json', 2, 0, 0],
+            ["Edge with two conditions", 'test/whisker/model/util/SimpleGraph-multiple-edge-conditions.json', 2, 0, 0],
+            ["Duplicated graph id", 'test/whisker/model/util/SimpleGraph-error-dup-graph-id.json', 2, 0, 0],
+            ["Duplicated edge id", 'test/whisker/model/util/SimpleGraph-error-dup-edge-id.json', 1, 0, 0],
+            ["No edge id.", 'test/whisker/model/util/SimpleGraph-noterror-no-edge-id.json', 2, 0, 0]
+        ];
+        it.each(table)('%s',
+            (name: string, file: string, pmCount: number, umCount: number, otemCount: number) => {
+                const text = readFileSync(file, 'utf8');
+                const loader = new ModelLoader();
+                const result = loader.loadModels(text);
+                expect(result.programModels.length).toBe(pmCount);
+                expect(result.userModels.length).toBe(umCount);
+                expect(result.onTestEndModels.length).toBe(otemCount);
+            });
     });
 
-    test('Edge with two conditions', () => {
-        const text = readFileSync('test/whisker/model/util/SimpleGraph-multiple-edge-conditions.json', 'utf8');
-        const loader = new ModelLoader();
-        expect(loader.loadModels(text)).not.toBeNull();
-    });
-
-    test('Duplicated graph id', () => {
-        const text = readFileSync('test/whisker/model/util/SimpleGraph-error-dup-graph-id.json', 'utf8');
-        const loader = new ModelLoader();
-        expect(function () {
-            loader.loadModels(text);
-        }).not.toThrow();
-    });
-
-    test('Duplicated node id', () => {
-        const text = readFileSync('test/whisker/model/util/SimpleGraph-error-dup-node-id.json', 'utf8');
-        const loader = new ModelLoader();
-        expect(function () {
-            loader.loadModels(text);
-        }).toThrow();
-    });
-
-    test('Duplicated edge id', () => {
-        const text = readFileSync('test/whisker/model/util/SimpleGraph-error-dup-edge-id.json', 'utf8');
-        const loader = new ModelLoader();
-        expect(function () {
-            loader.loadModels(text);
-        }).not.toThrow();
-    });
-
-    test('No condition on edge.', () => {
-        const text = readFileSync('test/whisker/model/util/SimpleGraph-error-no-edge-condition.json', 'utf8');
-        const loader = new ModelLoader();
-        expect(function () {
-            loader.loadModels(text);
-        }).toThrow();
-    });
-
-    test('No start node given.', () => {
-        const text = readFileSync('test/whisker/model/util/SimpleGraph-error-no-startnode.json', 'utf8');
-        const loader = new ModelLoader();
-        expect(function () {
-            loader.loadModels(text);
-        }).toThrow();
-    });
-
-    test('Two start nodes given.', () => {
-        const text = readFileSync('test/whisker/model/util/SimpleGraph-error-two-startnodes.json', 'utf8');
-        const loader = new ModelLoader();
-        expect(function () {
-            loader.loadModels(text);
-        }).toThrow();
-    });
-
-    test('Unknown end node of edge.', () => {
-        const text = readFileSync('test/whisker/model/util/SimpleGraph-error-unknown-node1.json', 'utf8');
-        const loader = new ModelLoader();
-        expect(function () {
-            loader.loadModels(text);
-        }).toThrow();
-    });
-
-    test('Unknown start node of edge.', () => {
-        const text = readFileSync('test/whisker/model/util/SimpleGraph-error-unknown-node2.json', 'utf8');
-        const loader = new ModelLoader();
-        expect(function () {
-            loader.loadModels(text);
-        }).toThrow();
-    });
-
-    test('Edge condition type wrong.', () => {
-        const text = readFileSync('test/whisker/model/util/SimpleGraph-error-edge-condition.json', 'utf8');
-        const loader = new ModelLoader();
-        expect(function () {
-            loader.loadModels(text);
-        }).toThrow();
-    });
-
-    test('No node id', () => {
-        const text = readFileSync('test/whisker/model/util/SimpleGraph-error-no-node-id.json', 'utf8');
-        const loader = new ModelLoader();
-        expect(function () {
-            loader.loadModels(text);
-        }).toThrow();
-    });
-
-    test('No edge id.', () => {
-        const text = readFileSync('test/whisker/model/util/SimpleGraph-noterror-no-edge-id.json', 'utf8');
-        const loader = new ModelLoader();
-        expect(function () {
-            loader.loadModels(text);
-        }).not.toThrow();
+    describe('Loading invalid Models fails', () => {
+        const table: [string, string][] = [
+            ["Duplicated node id", 'test/whisker/model/util/SimpleGraph-error-dup-node-id.json'],
+            ["No condition on edge.", 'test/whisker/model/util/SimpleGraph-error-no-edge-condition.json'],
+            ["No start node given.", 'test/whisker/model/util/SimpleGraph-error-no-startnode.json'],
+            ["Two start nodes given.", 'test/whisker/model/util/SimpleGraph-error-two-startnodes.json'],
+            ["Unknown end node of edge.", 'test/whisker/model/util/SimpleGraph-error-unknown-node1.json'],
+            ["Unknown start node of edge.", 'test/whisker/model/util/SimpleGraph-error-unknown-node2.json'],
+            ["Edge condition type wrong.", 'test/whisker/model/util/SimpleGraph-error-edge-condition.json'],
+            ["No node id", 'test/whisker/model/util/SimpleGraph-error-no-node-id.json']
+        ];
+        it.each(table)('%s', (name: string, file: string) => {
+            const text = readFileSync(file, 'utf8');
+            const loader = new ModelLoader();
+            expect(function () {
+                loader.loadModels(text);
+            }).toThrow();
+        });
     });
 });
