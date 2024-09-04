1c517e0d7ff339cf2d227880d06c285e6d615778
Dani Lipp <lipp13@ads.uni-passau.de>
2024-09-04 13:48:23 +0200

    refactored tests for ModelNode.test.ts

--- whisker-main/test/whisker/model/components/ModelNode.test.ts
+++ whisker-main/test/whisker/model/components/ModelNode.test.ts
@@ -1,35 +1,55 @@
-import {ModelNode} from "../../../../src/whisker/model/components/ModelNode";
+import {ModelNode, SimpleModelNode} from "../../../../src/whisker/model/components/ModelNode";
 import {ProgramModelEdge} from "../../../../src/whisker/model/components/ModelEdge";
 
 describe('Model node', () => {
-    test("constructor", () => {
-        expect(() => {
-            new ModelNode("id", "label");
-        }).not.toThrow();
+    test("constructor throws for undefined id", () => {
         expect(() => {
             new ModelNode(undefined, "label");
         }).toThrow();
+    });
+
+    test("constructor with undefined label works", () => {
         const node = new ModelNode("id", undefined);
         expect(node.label).toBe("id");
     });
 
-    test("Model node functions", () => {
+    test("Can't add edge with wrong from node", () => {
         const edge = new ProgramModelEdge("id", "label", "graphID", "from", "to", 1000, -1);
-        let node = new ModelNode("id", "label");
+        const node = new ModelNode("id", "label");
         expect(() => {
             node.addOutgoingEdge(edge);
         }).toThrow();
-        node = new ModelNode("from", "label");
-        expect(() => {
-            node.addOutgoingEdge(edge);
-        }).not.toThrow();
-        expect(node.edges.length).toBe(1);
+    });
 
-        expect(() => {
-            node.reset();
-        }).not.toThrow();
-        expect(() => {
-            node.simplifyForSave();
-        }).not.toThrow();
+    test("Model add a valid edge increases the amount of edges", () => {
+        const edge = new ProgramModelEdge("id", "label", "graphID", "from", "to", 1000, -1);
+        const node = new ModelNode("from", "label");
+        node.addOutgoingEdge(edge);
+        expect(node.edges.length).toBe(1);
+    });
+
+    test("reset() calls reset() of edges", () => {
+        const edge1 = new ProgramModelEdge("id", "label", "graphID", "from", "to", 1000, -1);
+        const edge2 = new ProgramModelEdge("id", "label", "graphID", "from", "to2", 1000, -1);
+        const node = new ModelNode("from", "label");
+        node.addOutgoingEdge(edge1);
+        node.addOutgoingEdge(edge2);
+        edge1.lastTransition = 1;
+        edge2.lastTransition = 2;
+        node.reset();
+        expect(edge1.lastTransition).toBe(0);
+        expect(edge2.lastTransition).toBe(0);
+    });
+
+    test("SimplifyForSave", () => {
+        const edge = new ProgramModelEdge("id", "label", "graphID", "from", "to", 1000, -1);
+        const node = new ModelNode("from", "label");
+        node.addOutgoingEdge(edge);
+        const actual = node.simplifyForSave();
+        const expected: SimpleModelNode = {
+            id: "from",
+            label: "label"
+        };
+        expect(actual).toStrictEqual(expected);
     });
 });
