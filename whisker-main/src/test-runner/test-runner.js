49bc4dcf8d123e86436d3f7bd8c329b459cb11d5
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2024-11-07 15:07:35 +0100

    fix(TestRunner): Work around "Test case not executed" errors

--- whisker-main/src/test-runner/test-runner.js
+++ whisker-main/src/test-runner/test-runner.js
@@ -543,29 +543,27 @@ class TestRunner extends EventEmitter {
      * @param props
      * @returns {Promise<void>}
      */
-    async _determineCoverages(test, props){
+    async _determineCoverages(test, props) {
+        if (!(props.traceBlockCoverage && props.traceBranchCoverage)) {
+            return;
+        }
+
         // Set the execution trace and the covered blocks set for computing coverages.
         const coverageTrace = this.vmWrapper.vm.getTraces();
+        test.coverage = coverageTrace.blockCoverage;
+        test.trace = new ExecutionTrace(coverageTrace.branchDistances, []);
 
-        if (props.traceBlockCoverage) {
-            test.coverage = coverageTrace.blockCoverage;
-
-            // Infer statement coverage
-            for (const statement of this.statementMap.keys()) {
-                if (await statement.isCovered(test)) {
-                    this.statementMap.set(statement, true);
-                }
+        // Infer statement coverage
+        for (const statement of this.statementMap.keys()) {
+            if (await statement.isCovered(test)) {
+                this.statementMap.set(statement, true);
             }
         }
 
-        if (props.traceBranchCoverage) {
-            test.trace = new ExecutionTrace(coverageTrace.branchDistances, []);
-
-            // Infer branch coverage
-            for (const branch of this.branchMap.keys()) {
-                if (await branch.isCovered(test)) {
-                    this.branchMap.set(branch, true);
-                }
+        // Infer branch coverage
+        for (const branch of this.branchMap.keys()) {
+            if (await branch.isCovered(test)) {
+                this.branchMap.set(branch, true);
             }
         }
     }
