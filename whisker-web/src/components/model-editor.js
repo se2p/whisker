0f58ccf3205de266e21258fd743a3329410202d1
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2024-12-02 15:22:47 +0100

    fix(whisker-web): fix #352
    In the model-based editor, avoid duplicating entries in the dropdown
    menu of available checks when editing a check the second time.

--- whisker-web/src/components/model-editor.js
+++ whisker-web/src/components/model-editor.js
@@ -1142,7 +1142,13 @@ class ModelEditor {
             }
         }
 
+        const currentOptions = new Set($(`${ModelEditor.CHECK_CHOOSER} option`).map((_, option) => $(option).val()));
+
         checkNames.forEach(name => {
+            if (currentOptions.has(name)) {
+                return;
+            }
+
             const key = `modelEditor:${name}`;
             $(ModelEditor.CHECK_CHOOSER).append($('<option/>', {'value': name, 'data-i18n': key}).text(i18n.t(key)));
         });
