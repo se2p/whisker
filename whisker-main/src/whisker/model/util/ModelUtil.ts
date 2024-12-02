99d0251ee34ec1fe943cbad91267154664f61199
Dani Lipp <lipp13@ads.uni-passau.de>
2024-12-02 09:11:32 +0100

    refactored the number conversion to a common method

--- whisker-main/src/whisker/model/util/ModelUtil.ts
+++ whisker-main/src/whisker/model/util/ModelUtil.ts
@@ -156,8 +156,21 @@ export abstract class ModelUtil {
      * Test whether a value is a number.
      */
     static testNumber(value: ParamType): number {
-        if (value == null || value === '' || isNaN(Number(value))) {
+        const result = this.returnNumberIfPossible(value);
+        if (result == null) {
             throw new NotANumericalValueError(String(value));
+        }
+        return result;
+    }
+
+    /**
+     * Returns the value as a number if possible or null otherwise.
+     * @param value The value to be converted to a number
+     * @return The input converted to a number
+     */
+    private static returnNumberIfPossible(value: ParamType): number | null {
+        if (value == null || value === '' || isNaN(Number(value))) {
+            return null;
         }
         return Number(value.toString());
     }
@@ -449,8 +462,8 @@ export abstract class ModelUtil {
     }
 
     static getNumberFunction(text: ArgType, t: TestDriver): () => number {
-        const asNumber = Number(text);
-        if (Number.isNaN(asNumber)) {
+        const asNumber = this.returnNumberIfPossible(text);
+        if (asNumber == null) {
             const func = ModelUtil.getExpressionForEval(t, text).expr;
             return () => ModelUtil.testNumber(Number(ModelUtil.evaluateExpression(t, func)));
         } else {
