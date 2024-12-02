b25bd710ca369d06f369e9246e47e26bb0a6dd6d
Daniel Lipp <lipp@fim.uni-passau.de>
2024-12-02 08:50:38 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Sebastian Schweikl <schweikl@fim.uni-passau.de>

--- whisker-main/src/whisker/model/components/Check.ts
+++ whisker-main/src/whisker/model/components/Check.ts
@@ -60,7 +60,7 @@ export class Check {
         this._id = id;
         this._edgeLabel = edgeLabel;
 
-        if((name == "Expr" || name == "Function") && args.length > 1) {
+        if (name == "Expr" || name == "Function") {
             this._args = [args.join(" ")];
         }
         let expectedLength: number;
