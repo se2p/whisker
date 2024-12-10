c906a7864a7834dc15b022b3e0b471fa4f3c5d35
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2024-12-10 19:35:16 +0100

    doc(changes.ts): clarify implementation logic

--- whisker-main/src/whisker/model/checks/changes.ts
+++ whisker-main/src/whisker/model/checks/changes.ts
@@ -51,6 +51,7 @@ export interface ChangingCheck<T extends Change = Change> {
 function isNumCheck(check: ChangingCheck): check is ChangingCheck<number> {
     return typeof check.change === "number";
 }
+
 /**
  * Tells whether the two given checks contradict each other. Every check represents a set of allowed number values.
  * Two checks are contradicting if the corresponding sets are disjoint.
@@ -59,9 +60,13 @@ function isNumCheck(check: ChangingCheck): check is ChangingCheck<number> {
  * @param check2 The second check
  */
 export function contradicts(check1: ChangingCheck, check2: ChangingCheck): boolean {
+    // If given two number checks, we try to determine if there's a contradiction on the precise "number"-level.
     if (isNumCheck(check1) && isNumCheck(check2)) {
         return contradictsNum(check1, check2);
     }
+
+    // Otherwise, we have at least one operator check. Then, we have to convert the other check to the same level
+    // if necessary, and try to determine a contradiction on the coarser "operator"-level.
 
     if (isNumCheck(check1)) {
         return contradicts(toOpCheck(check1), check2);
@@ -79,13 +84,17 @@ function contradictsNum(check1: ChangingCheck<number>, check2: ChangingCheck<num
     const {negated: negated2, change: num2} = check2;
 
     if (negated1 && negated2) {
+        // Essentially, we have two singleton sets, and create their complements in R (real numbers), before
+        // intersecting them. This set is never empty, hence no contradiction possible.
         return false;
     }
 
     if (negated1 || negated2) {
+        // Exactly one of the two is negated. Contradiction only possible if both numbers are the same.
         return num1 === num2;
     }
 
+    // No negation. Contradiction only possible if the numbers are not the same.
     return num1 !== num2;
 }
 
@@ -95,17 +104,17 @@ function contradictsOp(check1: ChangingCheck<ChangeOp>, check2: ChangingCheck<Ch
 
     switch (op1) {
         case "+":
-            return ["=", "-", "-="].includes(op2);
+            return ["=", "-", "-="].includes(op2); // All the operators that don't allow for an increase.
         case "-":
-            return ["=", "+", "+="].includes(op2);
+            return ["=", "+", "+="].includes(op2); // All the operators that don't allow for a decrease.
         case "=":
-            return ["+", "-", "!="].includes(op2);
+            return ["+", "-", "!="].includes(op2); // All the operators that don't allow for staying the same.
         case "+=":
-            return op2 === "-";
+            return op2 === "-"; // The only operator allowing neither increase nor staying the same.
         case "-=":
-            return op2 === "+";
+            return op2 === "+"; // The only operation allowing neither decrease nor staying the same.
         case "!=":
-            return op2 === "=";
+            return op2 === "="; // The only operator allowing neither increase nor decrease.
         default:
             throw new NonExhaustiveCaseDistinction(op1);
     }
