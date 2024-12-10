8892cf1a03f7967b4970258c51534d3ab07d2797
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2024-12-10 14:44:41 +0100

    test(checks): Fix expected outcome for checks

--- whisker-main/test/whisker/model/components/Check.test.ts
+++ whisker-main/test/whisker/model/components/Check.test.ts
@@ -766,8 +766,8 @@ describe('Effect', () => {
                 ["AttrChange", true, ['sprite', 'var', '-'], "AttrChange", true, ['sprite', 'var', '-='], false],
 
                 // different values
-                ["VarChange", true, ['sprite', 'var', '-5'], "VarChange", true, ['sprite', 'var', '-7'], true],
-                ["VarChange", true, ['sprite', 'var', '+5'], "VarChange", true, ['sprite', 'var', '+7'], true],
+                ["VarChange", true, ['sprite', 'var', '-5'], "VarChange", true, ['sprite', 'var', '-7'], false],
+                ["VarChange", true, ['sprite', 'var', '+5'], "VarChange", true, ['sprite', 'var', '+7'], false],
                 ["AttrChange", false, ['sprite', 'var', '-5'], "AttrChange", false, ['sprite', 'var', '-7'], true],
                 ["AttrChange", false, ['sprite', 'var', '+5'], "AttrChange", false, ['sprite', 'var', '+7'], true],
             ];
