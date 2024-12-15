4b9a962cfe4c24d8bd96e2afde4e72e21e8a108b
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2024-12-15 10:34:15 +0100

    feat(model/inputs): skeletal implementation of factory function

--- whisker-main/src/whisker/model/inputs/newInput.ts
+++ whisker-main/src/whisker/model/inputs/newInput.ts
@@ -1,9 +1,10 @@
-import {ClickSprite} from "./ClickSprite";
-import {ClickStage} from "./ClickStage";
-import {InputKey} from "./InputKey";
-import {InputText} from "./InputText";
-import {MouseDown} from "./MouseDown";
-import {MouseMove} from "./MouseMove";
+import {ClickSprite, ClickSpriteJSON} from "./ClickSprite";
+import {ClickStage, ClickStageJSON} from "./ClickStage";
+import {InputKey, InputKeyJSON} from "./InputKey";
+import {InputText, InputTextJSON} from "./InputText";
+import {MouseDown, MouseDownJSON} from "./MouseDown";
+import {MouseMove, MouseMoveJSON} from "./MouseMove";
+import {NonExhaustiveCaseDistinction} from "../../core/exceptions/NonExhaustiveCaseDistinction";
 
 export type Input =
     | ClickSprite
@@ -13,3 +14,32 @@ export type Input =
     | MouseDown
     | MouseMove
     ;
+
+export type InputJSON =
+    | ClickSpriteJSON
+    | ClickStageJSON
+    | InputKeyJSON
+    | InputTextJSON
+    | MouseDownJSON
+    | MouseMoveJSON
+    ;
+
+export function newInput(inputJSON: InputJSON): Input {
+    const name = inputJSON.name;
+    switch (name) {
+        case "InputClickSprite":
+            return new ClickSprite();
+        case "InputClickStage":
+            return new ClickStage();
+        case "InputKey":
+            return new InputKey();
+        case "InputText":
+            return new InputKey();
+        case "InputMouseDown":
+            return new MouseDown();
+        case "InputMouseMove":
+            return new MouseMove();
+        default:
+            throw new NonExhaustiveCaseDistinction(name);
+    }
+}
