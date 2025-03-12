4dde89339c6ea241c99af74398f850e5b4ce6a89
Dani Lipp <lipp13@ads.uni-passau.de>
2025-03-12 19:51:33 +0100

    test(model): decreased repetitions to 1

--- whisker-web/test/model/model.test.js
+++ whisker-web/test/model/model.test.js
@@ -165,7 +165,7 @@ describe('Model tests on multiple events per step', () => {
         await loadProject('test/model/scratch-programs/fruitcatcher.sb3',
             'test/model/model-jsons/fruitcatcher-random-fruit.json');
         await page.evaluate(factor => document.querySelector('#model-duration').value = factor, 20);
-        await page.evaluate(factor => document.querySelector('#model-repetitions').value = factor, 3);
+        await page.evaluate(factor => document.querySelector('#model-repetitions').value = factor, 1);
 
         const startTestButton = await page.$('#run-all-tests');
         await startTestButton.click();
