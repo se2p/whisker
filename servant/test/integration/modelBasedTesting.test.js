39ff0806f8b65b005a80309e65e098fd005e14bf
Dani Lipp <lipp13@ads.uni-passau.de>
2025-03-12 22:25:15 +0100

    test(modelBasedTesting): updated coverage results

--- servant/test/integration/modelBasedTesting.test.js
+++ servant/test/integration/modelBasedTesting.test.js
@@ -18,36 +18,36 @@ describe('Servant: Model-Based Testing', () => {
         const expectedCoverage =
 `Coverage:
  # coverage:
-#   combined: 0.85 (46/54)
+#   combined: 0.65 (35/54)
 #   individual:
 #     Stage: 1.00 (6/6)
-#     Bowl: 0.80 (8/10)
-#     Apple: 0.93 (14/15)
-#     Bananas: 0.78 (18/23)`
+#     Bowl: 0.70 (7/10)
+#     Apple: 0.60 (9/15)
+#     Bananas: 0.57 (13/23)`
 
         const expectedModelCoverage =
 `Model coverage:
  # modelCoverage:
-#   combined: 0.80 (66/82)
+#   combined: 0.48 (39/82)
 #   individual:
-#     bowl: 0.67 (4/6)
-#     apple: 1.00 (4/4)
-#     appleStop: 0.80 (4/5)
-#     bananasR: 0.83 (5/6)
-#     bananasB: 0.83 (5/6)
+#     bowl: 0.50 (3/6)
+#     apple: 0.25 (1/4)
+#     appleStop: 0.40 (2/5)
+#     bananasR: 0.50 (3/6)
+#     bananasB: 0.33 (2/6)
 #     init: 1.00 (2/2)
 #     timer: 0.50 (1/2)
 #     timer2: 1.00 (3/3)
-#     points: 0.91 (10/11)
-#     spawnApple: 1.00 (3/3)
+#     points: 0.27 (3/11)
+#     spawnApple: 0.67 (2/3)
 #     spawnBanan: 1.00 (1/1)
 #     bananFalling: 1.00 (2/2)
-#     threeApples: 0.75 (3/4)
-#     threeBananas: 0.75 (3/4)
+#     threeApples: 0.25 (1/4)
+#     threeBananas: 0.25 (1/4)
 #     appleState: 1.00 (2/2)
 #     bananState: 1.00 (3/3)
-#     bananas2: 0.71 (5/7)
-#     bananOnRed: 0.57 (4/7)
+#     bananas2: 0.43 (3/7)
+#     bananOnRed: 0.29 (2/7)
 #     bowlMove: 1.00 (2/2)
 #     end: 0.00 (0/2)`;
 
