d66d4cd3c67bb1c3016d7918d3334c8ebda00ee5
Dani Lipp <lipp13@ads.uni-passau.de>
2025-03-13 13:12:07 +0100

    fix(test-runner): restored original inputs before refactoring

--- whisker-main/src/test-runner/test-runner.js
+++ whisker-main/src/test-runner/test-runner.js
@@ -141,7 +141,7 @@ class TestRunner extends EventEmitter {
             const res = await this._executeTests(vm, tests, modelTester, props, modelProps,
                 resultRecords, testStatusResults, testResults,
                 startTime, projectName, totalAssertions,
-                600000, true);
+                0, true);
             if (res == null) {
                 return null;
             }
