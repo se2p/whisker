22900f6ac354c97299d3d2e7d25dfe5ce06152eb
Dani Lipp <lipp13@ads.uni-passau.de>
2025-03-19 09:46:16 +0100

    feature(PointsTo): check is registered in CheckUtility

--- whisker-main/src/whisker/model/checks/PointsTo.ts
+++ whisker-main/src/whisker/model/checks/PointsTo.ts
@@ -54,25 +54,29 @@ export class PointsTo extends AbstractCheck<PointsToJSON, CheckFun0> {
         if (this._args[1] != "_mouse_") {
             ModelUtil.checkSpriteExistence(t, this._args[1]).name;
         }
+
+        const check = (s: Sprite) => {
+            let expectedDirection: number, hasCorrectDirection: boolean;
+            if (this._args[1] == "_mouse_") {
+                expectedDirection = ModelUtil.getExpectedDirectionForSpriteLookingAtMouse(s, t);
+                hasCorrectDirection = ModelUtil.checkDirectionWithinDelta(s, expectedDirection);
+            } else {
+                const target = t.getSprite(this._args[1]);
+                expectedDirection = ModelUtil.getExpectedDirectionForSprite1LookingAtSprite2(s, target);
+                hasCorrectDirection = ModelUtil.checkDirectionWithinDelta(s, expectedDirection);
+                if (!hasCorrectDirection) {
+                    // maybe the sprite just moved so it did point to the sprite
+                    const dirOld = ModelUtil.getExpectedDirectionForSprite1LookingAtSprite2(s, target.old);
+                    hasCorrectDirection = ModelUtil.checkDirectionWithinDelta(s, dirOld);
+                }
+            }
+            return result(hasCorrectDirection, {actual: s.direction, expected: expectedDirection});
+        };
+
+        cu.registerOnVisualChange(spriteNameRotate, this, graphID, check);
+
         return () => {
             const sprites = t.getSprite(spriteNameRotate).getClones(true);
-            const check = (s: Sprite) => {
-                let expectedDirection: number, hasCorrectDirection: boolean;
-                if (this._args[1] == "_mouse_") {
-                    expectedDirection = ModelUtil.getExpectedDirectionForSpriteLookingAtMouse(s, t);
-                    hasCorrectDirection = ModelUtil.checkDirectionWithinDelta(s, expectedDirection);
-                } else {
-                    const target = t.getSprite(this._args[1]);
-                    expectedDirection = ModelUtil.getExpectedDirectionForSprite1LookingAtSprite2(s, target);
-                    hasCorrectDirection = ModelUtil.checkDirectionWithinDelta(s, expectedDirection);
-                    if (!hasCorrectDirection) {
-                        // maybe the sprite just moved so it did point to the sprite
-                        const dirOld = ModelUtil.getExpectedDirectionForSprite1LookingAtSprite2(s, target.old);
-                        hasCorrectDirection = ModelUtil.checkDirectionWithinDelta(s, dirOld);
-                    }
-                }
-                return result(hasCorrectDirection, {actual: s.direction, expected: expectedDirection});
-            };
             return any(check, this.negated, sprites);
         };
     }
