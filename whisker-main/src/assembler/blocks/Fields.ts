b040344983544bfffde14e829133d662c1976aed
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2025-03-21 19:37:52 +0100

    feat(Field): Flesh out Fields type

--- whisker-main/src/assembler/blocks/Fields.ts
+++ whisker-main/src/assembler/blocks/Fields.ts
@@ -1,3 +1,4 @@
+
 /**
  * Fields are text boxes, drop-down menus, etc. These are used directly in blocks where there is an input into which
  * one cannot drop a reporter. Fields are encoded as arrays. The first element of each array is the field's value
@@ -7,8 +8,15 @@
  * https://en.scratch-wiki.info/wiki/Dropdown_Menu
  */
 export type Fields = {
-
+    [fieldID in FieldKey]?: Field;
 }
+
+export type Field = Field1 | Field2;
+
+export type Field1 = [value: string] | [value: string, elementID: null];
+
+// Only used for custom variables, custom lists, and broadcasts.
+export type Field2 = [value: string, elementID: string];
 
 // Rectangular drop-down menus
 const noShadowFieldKeys = [
