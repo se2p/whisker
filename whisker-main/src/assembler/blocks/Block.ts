66e4abe6e609002f31aa60e5d60f63437ea6a07f
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2025-03-21 20:59:08 +0100

    feat(Blocks): Interfaces and type guards for stackable blocks

--- whisker-main/src/assembler/blocks/Block.ts
+++ whisker-main/src/assembler/blocks/Block.ts
@@ -1,6 +1,20 @@
-import {Opcode} from "./Opcode";
-import {Inputs, TopLevelListBlock, TopLevelVariableBlock} from "./Inputs";
+import {Inputs, isTopLevelDataBlock, TopLevelListBlock, TopLevelVariableBlock} from "./Inputs";
+import {
+    capBlockOpcodes,
+    cBlockOpcodes,
+    dropDownMenuOpcodes,
+    hatBlockOpcodes,
+    Opcode,
+    ShadowBlockOpcode,
+    shadowBlockOpcodes,
+    stackBlockOpcodes
+} from "./Opcode";
+import {isStackBlock, StackBlock} from "./shapes/StackBlock";
+import {CBlock, isCBlock} from "./shapes/CBlock";
+import {CapBlock, isCapBlock} from "./shapes/CapBlock";
+import {HatBlock, isHatBlock} from "./shapes/HatBlock";
 import {Fields} from "./Fields";
+import {isShadowBlock, ShadowBlock} from "./other/ShadowBlock";
 
 export type BlockID = string;
 export type CommentID = string;
@@ -105,6 +119,30 @@ export function isTopLevelBlock(block: Block): block is TopLevelBlock {
     return block.topLevel && block.parent === null && block['x'] !== undefined && block['y'] !== undefined;
 }
 
+/**
+ * Tells whether the given block represents an oval-shaped dropdown menu that is currently obscured by another input,
+ * e.g., a variable block, or another reporter block.
+ *
+ * @param block the block to check
+ */
+export function isObscuredDropDownMenu(block: Block): boolean {
+    /*
+     * This is a funny one. When obscuring an oval-shaped dropdown menu with another input, Scratch sets its parent to
+     * null. Then, the dropdown menu no longer knows which block it belonged to, thus effectively disabling it. However,
+     * only toplevel blocks can have null as parent. So, Scratch also has to set toplevel = true, and add useless x and
+     * y properties. To avoid drawing the dropdown menu as separate block, it also sets the shadow property to true.
+     * (Regardless of whether the menu is obscured or not.) This indicates the block is just a fake block.
+     *
+     * "A shadow block is a reporter in an input for which one can enter or pick a value, and which cannot be dragged
+     * around but can be replaced by a normal reporter. Scratch internally considers these to be blocks, although they
+     * are not usually thought of as such. (These notions come from Blockly, which Scratch Blocks is based on.)"
+     *
+     * https://en.scratch-wiki.info/wiki/Scratch_File_Format#Blocks
+     */
+    return (dropDownMenuOpcodes as Readonly<Array<Opcode>>).includes(block.opcode) &&
+        block.shadow && isTopLevelBlock(block);
+}
+
 // eslint-disable-next-line @typescript-eslint/ban-types
 export function isBlock(o: {}): o is Block {
     return (
@@ -119,3 +157,42 @@ export function isBlock(o: {}): o is Block {
 export function isBlockID(x: unknown): x is BlockID {
     return typeof x === "string";
 }
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function isScratchBlock(o: {}): o is ScratchBlock {
+    return isBlock(o) || isTopLevelDataBlock(o);
+}
+
+/**
+ * A stackable block is a block which other stackable blocks can be connected to – above (`parent`) or below (`next`).
+ * That is, stackable blocks have a `parent`-`next`-relationship. This holds for all blocks except reporter blocks
+ * and shadow blocks. Although a reporter usually has a `parent` property, the parent is connected to the reporter using
+ * an `InputKey`, not `next`. The same holds for shadow blocks. Note that some stackable blocks (`HatBlock`) never have
+ * a `parent`, while others (`CapBlock`) never have a `next`.
+ */
+export type StackableBlock = Exclude<
+    | StackBlock
+    | CBlock
+    | CapBlock
+    | HatBlock
+    ,
+    ShadowBlock
+>;
+
+export function isStackableBlock(b: ScratchBlock): b is StackableBlock {
+    return !isShadowBlock(b) && (
+        isStackBlock(b) ||
+        isCBlock(b) ||
+        isCapBlock(b) ||
+        isHatBlock(b)
+    );
+}
+
+export type StackableBlockOpcode = typeof stackableBlockOpcodes[number];
+
+export const stackableBlockOpcodes = Object.freeze([
+    ...stackBlockOpcodes,
+    ...cBlockOpcodes,
+    ...capBlockOpcodes,
+    ...hatBlockOpcodes,
+].filter((opcode) => !shadowBlockOpcodes.includes(opcode as ShadowBlockOpcode)));
