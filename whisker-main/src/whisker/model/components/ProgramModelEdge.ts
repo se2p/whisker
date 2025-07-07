ccc5017678a29cc3dce17af52dfbfa3c1c47b8a0
Dani Lipp <lipp13@ads.uni-passau.de>
2025-07-07 08:55:30 +0200

    fix: changed method from "all" to "every"

--- whisker-main/src/whisker/model/components/ProgramModelEdge.ts
+++ whisker-main/src/whisker/model/components/ProgramModelEdge.ts
@@ -75,7 +75,7 @@ export class ProgramModelEdge extends AbstractEdge {
             return this.conditions.length === 0;
         }
 
-        return this.conditions.all(c => checks.includes(c) || c.check(stepsSinceLastTransition, stepsSinceEnd).passed);
+        return this.conditions.every(c => checks.includes(c) || c.check(stepsSinceLastTransition, stepsSinceEnd).passed);
     }
 
 
