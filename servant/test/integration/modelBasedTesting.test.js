8ded51a22aa597abcf545cdfc326a49bbbe612ab
Dani Lipp <lipp13@ads.uni-passau.de>
2025-08-14 15:25:38 +0200

    test: changed test to use new notation

--- servant/test/integration/modelBasedTesting.test.js
+++ servant/test/integration/modelBasedTesting.test.js
@@ -8,7 +8,7 @@ describe('Servant: Model-Based Testing', () => {
         let result = await runServant([
             'model',
             '-s whisker-web/test/model/scratch-programs/fruitcatcher.sb3',
-            '-u whisker-web/test/model/user-model-jsons/fruitcatcher-userModel.json',
+            '-t whisker-web/test/model/user-model-jsons/fruitcatcher-userModel.json',
             '-p whisker-web/test/model/model-jsons/fruitcatcher.json',
             '-n 3',
             '-r 1',
