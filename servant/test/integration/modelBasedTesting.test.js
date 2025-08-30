0f07c08391c357b6064ff0efe5a639e9a184beab
Dani Lipp <lipp13@ads.uni-passau.de>
2025-08-30 18:35:24 +0200

    test: fixed test and increased n from 3 to 5

--- servant/test/integration/modelBasedTesting.test.js
+++ servant/test/integration/modelBasedTesting.test.js
@@ -10,7 +10,7 @@ describe('Servant: Model-Based Testing', () => {
             '-s whisker-web/test/model/scratch-programs/Fruitcatcher.sb3',
             '-t whisker-web/test/model/user-model-jsons/Fruitcatcher-userModel.json',
             '-p whisker-web/test/model/model-jsons/Fruitcatcher.json',
-            '-n 3',
+            '-n 5',
             '-r 1',
             '--seed 123',
             '--acceleration Infinity'
@@ -19,38 +19,38 @@ describe('Servant: Model-Based Testing', () => {
         const expectedCoverage =
 `Coverage:
  # coverage:
-#   combined: 0.85 (46/54)
+#   combined: 0.94 (51/54)
 #   individual:
 #     Stage: 1.00 (6/6)
 #     Bowl: 0.80 (8/10)
-#     Apple: 0.93 (14/15)
-#     Bananas: 0.78 (18/23)`
+#     Apple: 1.00 (15/15)
+#     Bananas: 0.96 (22/23)`
 
         const expectedModelCoverage =
 `Model coverage:
  # modelCoverage:
-#   combined: 0.81 (67/83)
+#   combined: 0.76 (64/84)
 #   individual:
 #     bowl: 0.67 (4/6)
-#     apple: 1.00 (4/4)
-#     appleStop: 0.80 (4/5)
-#     bananasR: 0.83 (5/6)
+#     apple: 0.25 (1/4)
+#     appleStop: 1.00 (5/5)
+#     bananasR: 0.67 (4/6)
 #     bananasB: 0.83 (5/6)
 #     init: 1.00 (2/2)
 #     timer: 0.67 (2/3)
 #     timer2: 1.00 (3/3)
-#     points: 0.91 (10/11)
+#     points: 0.27 (3/11)
 #     spawnApple: 1.00 (3/3)
 #     spawnBanan: 1.00 (1/1)
 #     bananFalling: 1.00 (2/2)
-#     threeApples: 0.75 (3/4)
-#     threeBananas: 0.75 (3/4)
-#     appleState: 1.00 (2/2)
+#     threeApples: 1.00 (4/4)
+#     threeBananas: 1.00 (4/4)
+#     appleState: 1.00 (3/3)
 #     bananState: 1.00 (3/3)
-#     bananas2: 0.71 (5/7)
-#     bananOnRed: 0.57 (4/7)
+#     bananas2: 1.00 (7/7)
+#     bananOnRed: 0.71 (5/7)
 #     bowlMove: 1.00 (2/2)
-#     end: 0.00 (0/2)`;
+#     end: 0.50 (1/2)`;
 
 
         expect(result.error).toBeNull();
