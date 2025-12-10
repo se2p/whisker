624f156cb8d13f74c66ca223243f61a4e976ccbe
Dani Lipp <lipp13@ads.uni-passau.de>
2025-12-10 21:01:34 +0100

    feature: add missing clear coverage

--- whisker-web/src/index.js
+++ whisker-web/src/index.js
@@ -588,6 +588,8 @@ const _isNeatestSuite = function () {
 const runAllTests = async function () {
     $('#run-all-tests').tooltip('hide');
 
+    Whisker.modelTester.clearCoverage();
+
     if (Whisker.testFileSelect.files.length > 0 && Whisker.testFileSelect.getName().endsWith('.json')) {
         // Long tests, for example saved networks in Dynamic Suites, can take some time to be loaded;
         // Hence we wait a second before checking if tests are loaded.
