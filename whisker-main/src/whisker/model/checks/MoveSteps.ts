35cfbdade2adc022d888669ab07f1da5f3b33694
Dani Lipp <lipp13@ads.uni-passau.de>
2025-12-10 20:51:41 +0100

    fix: forgot to add changes for MoveSteps

--- whisker-main/src/whisker/model/checks/MoveSteps.ts
+++ whisker-main/src/whisker/model/checks/MoveSteps.ts
@@ -1,9 +1,11 @@
-import {CheckFun0, ICheckJSON, PureCheck, SlimCheckJSON} from "./AbstractCheck";
+import {BoundedCheck, CheckFun0, ICheckJSON, SlimCheckJSON} from "./AbstractCheck";
 import {z} from "zod";
 import TestDriver from "../../../test/test-driver";
 import {ArgType} from "../util/schema";
-import {NumberLike, parseNonUnionError, ParsingResult, SpriteName} from "./CheckTypes";
-import {movedCorrectAmountOfSteps} from "../util/ModelUtil";
+import {AttrName, NumberLike, parseNonUnionError, ParsingResult, SpriteName} from "./CheckTypes";
+import {getXYBounds, movedCorrectAmountOfSteps, XYBounds} from "../util/ModelUtil";
+import {result} from "./CheckResult";
+import Sprite from "../../../vm/sprite";
 
 const name = "MoveSteps" as const;
 
@@ -34,7 +36,7 @@ export const MoveStepsJSON = ICheckJSON.extend({
     args: MoveStepsArgs,
 });
 
-export class MoveSteps extends PureCheck<MoveStepsJSON, CheckFun0> {
+export class MoveSteps extends BoundedCheck<MoveStepsJSON, CheckFun0> {
 
     constructor(edgeLabel: string, json: SlimCheckJSON<MoveStepsJSON>) {
         super(edgeLabel, {...json, name});
@@ -42,6 +44,10 @@ export class MoveSteps extends PureCheck<MoveStepsJSON, CheckFun0> {
 
     public static convertArgs(args: ArgType[]): ParsingResult {
         return parseNonUnionError(MoveStepsArgs.safeParse(args));
+    }
+
+    protected get attrName(): AttrName {
+        return "x";
     }
 
     protected _validate(checkJSON: MoveStepsJSON): MoveStepsJSON {
@@ -58,8 +64,18 @@ export class MoveSteps extends PureCheck<MoveStepsJSON, CheckFun0> {
         const spriteName = sprite.name;
 
         this._registerOnMoveEvent(spriteName);
+        let bound = this._updatedBound(sprite, getXYBounds(sprite));
 
-        return () => movedCorrectAmountOfSteps(sprite, this._args[1]);
+        return () => {
+            const reason = {};
+            bound = this._updatedBound(sprite, bound);
+            const correct = movedCorrectAmountOfSteps(sprite, this._args[1], bound, reason);
+            return result(correct, reason, this.negated);
+        };
+    }
+
+    protected _updatedBound(s: Sprite, currentBounds: XYBounds): XYBounds {
+        return this._boundsNeedUpdate(s) ? getXYBounds(s) : currentBounds;
     }
 
     protected _contradicts(that: MoveSteps): boolean {
