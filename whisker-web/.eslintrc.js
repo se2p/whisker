561fa79d96f1ac10169e052919e4f34cee0f63a1
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2026-01-30 15:39:42 +0100

    chore(whisker-web/.eslintrc.js): Enable node env

--- whisker-web/.eslintrc.js
+++ whisker-web/.eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     root: true,
     extends: ['scratch', 'scratch/es6'],
     env: {
-        browser: true
+        browser: true,
+        node: true
     },
     overrides: [
         {
