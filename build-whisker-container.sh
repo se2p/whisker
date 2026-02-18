87fd50b06454128205b604adf0527e5687b1c547
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2026-02-18 14:32:40 +0100

    feat(build-whisker-docker.sh): Fall back to "regular" docker if necessary

--- build-whisker-container.sh
+++ build-whisker-container.sh
@@ -8,18 +8,26 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 IMG_TAG="whisker:${BRANCH}-${COMMIT}"
 
+# On Infosun workstations, use a different data root for docker. This avoids cluttering the Linux root partition.
+# On other systems, fall back to regular docker and its default data root location.
+with_local_data_root() {
+    if command -v dockerd-rootless-infosun &>/dev/null; then
+        dockerd-rootless-infosun --data-root "/local/${USER}/docker" -- "$@"
+    else
+        "$@"
+    fi
+}
+
 echo "🔨 Building Whisker Docker image ${IMG_TAG}"
-dockerd-rootless-infosun --data-root "/local/${USER}/docker" -- docker build . -t "${IMG_TAG}" -f Dockerfile --no-cache
+with_local_data_root docker build . -t "${IMG_TAG}" -f Dockerfile --no-cache
 
 echo "🔄 Converting to Apptainer SIF"
-dockerd-rootless-infosun --data-root "/local/${USER}/docker" -- apptainer build "${IMG_TAG}.sif" "docker-daemon://${IMG_TAG}"
+with_local_data_root apptainer build "${IMG_TAG}.sif" "docker-daemon://${IMG_TAG}"
 
 echo "🗑 Removing intermediate Docker image..."
-dockerd-rootless-infosun --data-root "/local/${USER}/docker" -- docker rmi "${IMG_TAG}"
+with_local_data_root docker rmi "${IMG_TAG}"
 
 echo "🧹 Cleaning up Docker system..."
-dockerd-rootless-infosun --data-root "/local/${USER}/docker" -- docker system prune -f
+with_local_data_root docker system prune -f
 
 echo "✅ Whisker image saved: ${IMG_TAG}.sif"
-
-
