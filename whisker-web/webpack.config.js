dca91a2df1af833deb524c57e60b374d40598c7b
Patric Feldmeier <feldmeie@fim.uni-passau.de>
2026-03-04 15:47:19 +0100

    Remove Caching of scratch-vm

--- whisker-web/webpack.config.js
+++ whisker-web/webpack.config.js
@@ -96,17 +96,6 @@ module.exports = [
             'whisker-gui': path.resolve(__dirname, 'src/index.js')
         },
 
-        // Enable persistent caching to decrease build times.
-        cache: {
-            type: 'filesystem',
-            buildDependencies: {
-                config: [__filename]
-            },
-            cacheDirectory: path.resolve(__dirname, '.webpack-cache-js'),
-            maxAge: 1000 * 60 * 60, // Clean cache after 1 hour
-            compression: 'gzip' // Compress cache files to save space
-        },
-
         output: {
             path: path.resolve(__dirname, 'dist'),
             filename: '[name].js'
